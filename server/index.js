62fd0a32fbb2fce3b185f3e10e459d28e054135e
Ahmed Shatat <a.shatat@hotmail.com>
2019-05-26 01:29:02 +0300

    chore: add kill process code in index.js
    #8

--- server/index.js
+++ server/index.js
@@ -5,3 +5,17 @@ const port = app.get('port');
 app.listen(port, () => {
   console.log(`server is running on port:${port}`);
 });
+
+// kill the process in dev mood
+if (process.env.NODE_ENV === 'development') {
+  // works with <ctrl> + C terminate
+  process.on('SIGINT', (sig) => {
+    console.log('Exit node process', sig);
+    process.exit(0);
+  })
+  // works with <ctrl> + Z susspend
+    .on('SIGTSTP', (sig) => {
+      console.log('Exit node process', sig);
+      process.exit(0);
+    });
+}
