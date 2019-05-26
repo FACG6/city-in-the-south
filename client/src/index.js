77f523c60a0e3ac422f2e84e9551c5d0250f64c2
Angham116 <angham.1161995@hotmail.com>
2019-05-26 15:26:52 +0300

    remove font-awesome from cdn and use npm modules

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import '@fortawesome/fontawesome-free/css/all.css';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
