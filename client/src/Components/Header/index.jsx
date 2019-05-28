dc308e509735a8ddf094393a3ddeb65708a5916e
AymanAlQoqa <aalqouqa@gmail.com>
2019-05-28 13:27:48 +0300

    add header for the not logged in user relates #13

--- client/src/Components/Header/index.jsx
+++ client/src/Components/Header/index.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import { Navbar, Nav, Col } from 'react-bootstrap';
+import './style.css';
 
-export default function Header() {
+export default function Header(props) {
+  const { islooged } = props;
+  const route = window.location.href.split('/')[3];
   return (
-    <>
-      <h1>header</h1>
-    </>
+    <div className="Navbar--container">
+      <Navbar collapseOnSelect expand="lg" variant="dark">
+        <Col className="ml-3 mr-5">
+          <Navbar.Brand href="#home" xs="auto" lg="2">
+            Work Together
+          </Navbar.Brand>
+
+          <img
+            src=""
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="logo"
+          />
+        </Col>
+
+        {!islooged && (
+          <div className="ml-auto">
+            <Nav className="ml-3">
+              <Nav.Link
+                href="/login"
+                className={`ml-3 nav--link ${route === 'login' &&
+                  'route--active'}`}
+              >
+                Login
+              </Nav.Link>
+              <Nav.Link
+                href="/signup"
+                className={`ml-3 mr-5 nav--link ${route === 'signup' &&
+                  'route--active'}`}
+              >
+                Signup
+              </Nav.Link>
+            </Nav>
+          </div>
+        )}
+      </Navbar>
+    </div>
   );
 }
