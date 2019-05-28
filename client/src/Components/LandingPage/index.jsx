8f61ab5c46836cfde862fb02b1c88a4cc5c191cf
Ahmed <ahmedalmi95@gmail.com>
2019-05-28 13:41:41 +0300

    made landing page

--- client/src/Components/LandingPage/index.jsx
+++ client/src/Components/LandingPage/index.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
-import UnderConstruction from '../UnderConstruction';
+import './style.css';
+import { Button, Alert } from 'react-bootstrap';
+import Slider from './Slider';
 
 export default function LandingPage() {
   return (
     <>
-      <h1>lANDING page</h1>
-      <UnderConstruction />
+      <div className="header">
+        <h2 className="header-tweet">
+          Want good (fair and meaningful) work or want to offer good work? Join
+          us and find/show opportunities to connect and work together.
+        </h2>
+      </div>
+      <div className="slider">
+        <Button variant="flat" size="xxl">
+          flat button
+        </Button>
+      </div>
+
+      <div className="about">
+        <Alert dismissible variant="danger">
+          <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+          <p>
+            Change this and that and try again. Duis mollis, est non commodo
+            luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+            Cras mattis consectetur purus sit amet fermentum.
+          </p>
+        </Alert>
+      </div>
+      <Slider />
     </>
   );
 }
