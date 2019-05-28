f05e876b25580ffeab663dbb968e95034aec0724
Angham116 <angham.1161995@hotmail.com>
2019-05-28 14:42:11 +0300

    feat: offerCard component #61

--- client/src/Components/Home/Offers/index.jsx
+++ client/src/Components/Home/Offers/index.jsx
@@ -1,6 +1,70 @@
 import React from 'react';
-import UnderConstruction from '../../UnderConstruction';
+import { Card } from 'react-bootstrap';
+import './style.css';
+import { withRouter } from 'react-router-dom';
 
-export default function Offers() {
-  return <UnderConstruction />;
+class OfferCard extends React.Component {
+  state = {
+    offer: null,
+    status: null,
+    hovered: null,
+  };
+
+  componentDidMount() {
+    // request for get all offers from backend (reponse: array of object as offerDetails)
+    const { offer } = this.props;
+    this.setState({ offer });
+  }
+
+  render() {
+    const { offer, hovered } = this.state;
+    const { hover } = this.props;
+    return (
+      <>
+        {offer ? (
+          <Card
+            className={`offer__card ${hovered}`}
+            key={offer[0].id}
+            onClick={() => console.log('object')}
+          >
+            <Card.Header className="offer__card--header">
+              <div>
+                <span className="offer__card--position">
+                  {offer[0].position}
+                </span>
+                <br />
+                <span className="offer__card--title">{offer[0].title}</span>
+              </div>
+              <div>
+                {hover
+                  ? () => this.setState({ hovered: 'card__hovered' })
+                  : null}
+                {hover ? (
+                  <span className="offer__card--status">
+                    {' '}
+                    {offer[0].status}
+                  </span>
+                ) : null}
+                <i
+                  className="far fa-bookmark offer__card--saved"
+                  onClick={this.handleSave}
+                />
+              </div>
+            </Card.Header>
+            <Card.Body>
+              <Card.Text className="card__description">
+                {offer[0].description}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        ) : (
+          <>
+            <span> loading... </span>
+          </>
+        )}
+      </>
+    );
+  }
 }
+
+export default withRouter(OfferCard);
