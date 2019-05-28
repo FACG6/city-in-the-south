ed517b3c6beab68e1eb8d4954215b7de1999351e
Ahmed <ahmedalmi95@gmail.com>
2019-05-28 13:37:11 +0300

    made slider for cards

---
+++ client/src/Components/LandingPage/Slider/index.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import Cards from "./../Cards";
+
+export default class ControlledCarousel extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleSelect = this.handleSelect.bind(this);
+
+    this.state = {
+      index: 0,
+      direction: null
+    };
+  }
+
+  handleSelect(selectedIndex, e) {
+    this.setState({
+      index: selectedIndex,
+      direction: e.direction
+    });
+  }
+
+  render() {
+    const { index, direction } = this.state;
+
+    return (
+      <Carousel
+        activeIndex={index}
+        direction={direction}
+        onSelect={this.handleSelect}
+      >
+        <Carousel.Item>
+        <Cards />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div>
+            <h1>hello header</h1>
+            <p>hell fodjfnsd </p>
+          </div>
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    );
+  }
+}
