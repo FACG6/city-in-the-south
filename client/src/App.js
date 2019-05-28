8ca61f55f285b5e06f2f8faeb108382167d21be1
Angham116 <angham.1161995@hotmail.com>
2019-05-28 12:08:58 +0300

    delete un necessary code

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PrivatePages from './PrivatePages';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 import {
   LandingPage,
