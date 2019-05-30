14a5354db8a9283e6333bc1f6298ee44c85cfd6a
AymanAlQoqa <aalqouqa@gmail.com>
2019-05-30 14:20:30 +0300

    add side card for skills and offer-type relates #22

---
+++ client/src/Components/OfferDetails/SideCard/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './style.css';
+
+const SideCard = props => {
+  const { title, items } = props;
+  return (
+    <>
+      <div className="side-card__title">
+        <span>{title}</span>
+      </div>
+      <ul>
+        {items
+          ? items.map(item => {
+              return (
+                <li key={items.indexOf(item)} className="side-card__li">
+                  <span> - </span>
+                  {item}
+                </li>
+              );
+            })
+          : null}
+      </ul>
+    </>
+  );
+};
+
+export default SideCard;
