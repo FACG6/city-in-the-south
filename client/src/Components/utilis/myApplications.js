0c38786c40a2e8671b3fe6069c9369a40faeb6c6
Angham116 <angham.1161995@hotmail.com>
2019-05-30 11:38:13 +0300

    feat: static data for saved offers

---
+++ client/src/Components/utilis/myApplications.js
@@ -0,0 +1,83 @@
+export default [
+  {
+    id: 1,
+    position: 'Marketing Manager',
+    title: 'Small Shop seeks workers',
+    description:
+      'We are looking to hire a Marketing Manager who will be in charge of overseeing the promotion of our companys brands.  As a successful hire, you will be  responsible  for developing pricing  strategies identify identifying new customers, supporting lead you will be  responsible  for developing pricing  strategies identify identifying new customers, supporting lead',
+    status: 'completed',
+    saved: true,
+  },
+  {
+    id: 2,
+    position: 'Accountant',
+    title: 'Buisness setup plan',
+    description:
+      ' Prepare balance sheets, profit and loss statements and other financial reports. Responsibilities also include analyzing trends, costs, revenues,  financial commitments and obligations incurred to predict future revenues and expenses. Reports',
+    status: 'in progress',
+    saved: true,
+  },
+  {
+    id: 3,
+    position: 'Vice President of Sales',
+    title: 'Voice artist for short video ',
+    description:
+      'We are looking to hire a Marketing Manager who will be in charge of overseeing the promotion of our companys brands.  As a successful hire, you will be  responsible  for developing pricing  strategies identify identifying new customers, supporting lead',
+    status: 'completed',
+    saved: false,
+  },
+  {
+    id: 4,
+    position: 'Database Adminstration',
+    title: 'Small Shop seeks workers',
+    description:
+      'We are looking to hire a Marketing Manager who will be in charge of overseeing the promotion of our companys brands.  As a successful hire, you will be  responsible  for developing pricing  strategies identify identifying new customers, supporting lead',
+    status: 'in progress',
+    saved: true,
+  },
+  {
+    id: 5,
+    position: 'Marketing Manager',
+    title: 'Small Shop seeks workers',
+    description:
+      'We are looking to hire a Marketing Manager who will be in charge of overseeing the promotion of our companys brands.  As a successful hire, you will be  responsible  for developing pricing  strategies identify identifying new customers, supporting lead',
+    status: 'completed',
+    saved: false,
+  },
+  {
+    id: 6,
+    position: 'Marketing Manager',
+    title: 'Small Shop seeks workers',
+    description:
+      'We are looking to hire a Marketing Manager who will be in charge of overseeing the promotion of our companys brands.  As a successful hire, you will be  responsible  for developing pricing  strategies identify identifying new customers, supporting lead',
+    status: 'active',
+    saved: false,
+  },
+  {
+    id: 7,
+    position: 'Vice President of Sales',
+    title: 'Voice artist for short video ',
+    description:
+      'We are looking to hire a Marketing Manager who will be in charge of overseeing the promotion of our companys brands.  As a successful hire, you will be  responsible  for developing pricing  strategies identify identifying new customers, supporting lead',
+    status: 'active',
+    saved: false,
+  },
+  {
+    id: 8,
+    position: 'Vice President of Sales',
+    title: 'Voice artist for short video ',
+    description:
+      'We are looking to hire a Marketing Manager who will be in charge of overseeing the promotion of our companys brands.  As a successful hire, you will be  responsible  for developing pricing  strategies identify identifying new customers, supporting lead',
+    status: 'pending',
+    saved: true,
+  },
+  {
+    id: 9,
+    position: 'Vice President of Sales',
+    title: 'Voice artist for short video ',
+    description:
+      'We are looking to hire a Marketing Manager who will be in charge of overseeing the promotion of our companys brands.  As a successful hire, you will be  responsible  for developing pricing  strategies identify identifying new customers, supporting lead',
+    status: 'active',
+    saved: false,
+  },
+];
