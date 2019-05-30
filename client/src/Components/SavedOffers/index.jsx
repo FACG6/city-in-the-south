fd3b805a919571e220f0c02361c9b21b2e0d1d28
Fatma Siam <fatma.o.siam@gmail.com>
2019-05-30 16:25:20 +0300

    feat: Saved offers Commponent Relates #30

--- client/src/Components/SavedOffers/index.jsx
+++ client/src/Components/SavedOffers/index.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
-import UnderConstruction from '../UnderConstruction';
+import React, { Component } from 'react';
+import { Container } from 'react-bootstrap';
+import OfferCard from '../CommonComponents/OfferCard';
+import offersDetails from './staticData';
+import './style.css';
 
-export default function SavedOffers() {
-  return (
-    <>
-      <h1>Saved Offers</h1>
-      <UnderConstruction />
-    </>
-  );
+export default class SavedOffers extends Component {
+  state = {
+    offers: null,
+  };
+
+  componentDidMount() {
+    this.setState({ offers: offersDetails });
+  }
+
+  render() {
+    const { offers } = this.state;
+    return (
+      <Container class="page__container">
+        <h1 className="saved-offer-title"> Saved Offers </h1>
+        {offers
+          ? offers.map(item => {
+              if (item.saved) {
+                return (
+                  <OfferCard
+                    hover
+                    offer={item}
+                    key={item.id}
+                    status={item.status}
+                  />
+                );
+              }
+            })
+          : null}
+      </Container>
+    );
+  }
 }
---
+++ client/src/Components/SavedOffers/staticData.js
@@ -0,0 +1,125 @@
+export default [
+  {
+    id: 1,
+    position: 'Marketing Manager',
+    title: 'Small Shop seeks workers',
+    description:
+      'We are looking to hire a Marketing Manager who will be in charge of overseeing the promotion of our companys brands.  As a successful hire, you will be  responsible  for developing pricing  strategies identify identifying new customers, supporting lead',
+    status: 'completed',
+    skill: [
+      { id: 1, name: 'javascript' },
+      { id: 2, name: 'node js' },
+      { id: 3, name: 'express js' },
+      { id: 5, name: 'python' },
+    ],
+    offer_type: [
+      { id: 1, name: 'fixed price' },
+      { id: 2, name: 'part time' },
+      { id: 3, name: 'full time' },
+    ],
+  },
+  {
+    id: 2,
+    position: 'Accountant',
+    title: 'Buisness setup plan',
+    description:
+      ' Prepare balance sheets, profit and loss statements and other financial reports. Responsibilities also include analyzing trends, costs, revenues,  financial commitments and obligations incurred to predict future revenues and expenses. Reports',
+    status: 'finished',
+    saved: true,
+    skill: [{ id: 2, name: 'node js' }],
+    offer_type: [
+      { id: 1, name: 'fixed price' },
+      { id: 2, name: 'part time' },
+      { id: 3, name: 'full time' },
+    ],
+  },
+  {
+    id: 3,
+    position: 'Vice President of Sales',
+    title: 'Voice artist for short video ',
+    description:
+      'We are looking to hire a Marketing Manager who will be in charge of overseeing the promotion of our companys brands.  As a successful hire, you will be  responsible  for developing pricing  strategies identify identifying new customers, supporting lead',
+    status: 'completed',
+    saved: false,
+    skill: [{ id: 1, name: 'javascript' }, { id: 5, name: 'python' }],
+    offer_type: [{ id: 1, name: 'fixed price' }],
+  },
+  {
+    id: 4,
+    position: 'Database Adminstration',
+    title: 'Small Shop seeks workers',
+    description:
+      'We are looking to hire a Marketing Manager who will be in charge of overseeing the promotion of our companys brands.  As a successful hire, you will be  responsible  for developing pricing  strategies identify identifying new customers, supporting lead',
+    status: 'completed',
+    saved: true,
+    skill: [{ id: 5, name: 'python' }],
+    offer_type: [{ id: 3, name: 'full time' }],
+  },
+  {
+    id: 5,
+    position: 'Marketing Manager',
+    title: 'Small Shop seeks workers',
+    description:
+      'We are looking to hire a Marketing Manager who will be in charge of overseeing the promotion of our companys brands.  As a successful hire, you will be  responsible  for developing pricing  strategies identify identifying new customers, supporting lead',
+    status: 'completed',
+    saved: false,
+    skill: [{ id: 2, name: 'node js' }, { id: 3, name: 'express js' }],
+    offer_type: [{ id: 2, name: 'part time' }, { id: 3, name: 'full time' }],
+  },
+  {
+    id: 6,
+    position: 'Marketing Manager',
+    title: 'Small Shop seeks workers',
+    description:
+      'We are looking to hire a Marketing Manager who will be in charge of overseeing the promotion of our companys brands.  As a successful hire, you will be  responsible  for developing pricing  strategies identify identifying new customers, supporting lead',
+    status: 'pending',
+    saved: true,
+    skill: [
+      { id: 2, name: 'node js' },
+      { id: 3, name: 'express js' },
+      { id: 5, name: 'python' },
+    ],
+    offer_type: [{ id: 1, name: 'fixed price' }, { id: 2, name: 'part time' }],
+  },
+  {
+    id: 7,
+    position: 'Vice President of Sales',
+    title: 'Voice artist for short video ',
+    description:
+      'We are looking to hire a Marketing Manager who will be in charge of overseeing the promotion of our companys brands.  As a successful hire, you will be  responsible  for developing pricing  strategies identify identifying new customers, supporting lead',
+    status: 'active',
+    saved: false,
+    skill: [
+      { id: 1, name: 'javascript' },
+      { id: 2, name: 'node js' },
+      { id: 5, name: 'python' },
+    ],
+    offer_type: [{ id: 1, name: 'fixed price' }, { id: 3, name: 'full time' }],
+  },
+  {
+    id: 8,
+    position: 'Vice President of Sales',
+    title: 'Voice artist for short video ',
+    description:
+      'We are looking to hire a Marketing Manager who will be in charge of overseeing the promotion of our companys brands.  As a successful hire, you will be  responsible  for developing pricing  strategies identify identifying new customers, supporting lead',
+    status: 'completed',
+    saved: true,
+    skill: [{ id: 1, name: 'javascript' }, { id: 5, name: 'python' }],
+    offer_type: [{ id: 2, name: 'part time' }, { id: 3, name: 'full time' }],
+  },
+  {
+    id: 9,
+    position: 'Vice President of Sales',
+    title: 'Voice artist for short video ',
+    description:
+      'We are looking to hire a Marketing Manager who will be in charge of overseeing the promotion of our companys brands.  As a successful hire, you will be  responsible  for developing pricing  strategies identify identifying new customers, supporting lead',
+    status: 'finished',
+    saved: true,
+    skill: [
+      { id: 1, name: 'javascript' },
+      { id: 2, name: 'node js' },
+      { id: 3, name: 'express js' },
+    ],
+    offer_type: [{ id: 1, name: 'fixed price' }, { id: 3, name: 'full time' }],
+  },
+];
