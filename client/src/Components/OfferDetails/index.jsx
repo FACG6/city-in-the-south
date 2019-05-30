3ac1bde47e558e2e7053bcf49c5f4533db5da031
AymanAlQoqa <aalqouqa@gmail.com>
2019-05-30 14:23:23 +0300

    modify offerDetails page relates #22

--- client/src/Components/OfferDetails/index.jsx
+++ client/src/Components/OfferDetails/index.jsx
@@ -1,6 +1,42 @@
-import React from 'react';
-import UnderConstruction from '../UnderConstruction';
+import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import './style.css';
+import offerData from '../utils/offer';
+import SideCard from './SideCard';
 
-export default function OfferDetails() {
-  return <UnderConstruction />;
+export default class OfferDetails extends Component {
+  state = {
+    // default value, should be removed if fetch offer details implemented
+    offer: {},
+  };
+
+  componentDidMount() {
+    const offerIdLength = window.location.href.split('/').length;
+    const offerId = window.location.href.split('/')[offerIdLength - 1];
+    // fetch offer details by offerId and save it in state
+    this.setState({ offer: offerData });
+  }
+
+  render() {
+    const { offer } = this.state;
+    return (
+      <Container className="page__container">
+        <div className="offer-details__header">
+          <span className="offer-details__position">{offer.position}</span>
+          <p className="offer-details__title">{offer.title}</p>
+        </div>
+        <Row className="offer-details__row">
+          <Col xs lg="9" className="offer-details__description">
+            <p>{offer.description}</p>
+          </Col>
+          <Col xs lg="2">
+            <div>
+              <SideCard title="skills" items={offer.skills} />
+              <SideCard title="offer type" items={offer.offer_type} />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
