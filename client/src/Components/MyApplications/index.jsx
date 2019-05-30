fee9e2eb34595735047bffe12546365092fa8817
AymanAlQoqa <aalqouqa@gmail.com>
2019-05-30 10:26:16 +0300

    modify MyApplications component to display my-applications cards relates #28

--- client/src/Components/MyApplications/index.jsx
+++ client/src/Components/MyApplications/index.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
-import UnderConstruction from '../UnderConstruction';
+import { Container } from 'react-bootstrap';
+import ApplicationCard from '../CommonComponents/OfferCard';
+import applications from '../utils/myApplications';
+import './style.css';
 
 export default function MyApplications() {
   return (
     <>
-      <h1>my applications</h1>
-      <UnderConstruction />
+      <Container className="my-applications__container">
+        <div className="my-applications__title">
+          <span>My Applications</span>
+        </div>
+        {applications
+          ? applications.map(item => {
+              if (item.status !== 'active') {
+                return (
+                  <ApplicationCard
+                    hover
+                    offer={item}
+                    key={item.id}
+                    saved={item.saved}
+                    status={item.status}
+                  />
+                );
+              }
+            })
+          : null}
+      </Container>
     </>
   );
 }
