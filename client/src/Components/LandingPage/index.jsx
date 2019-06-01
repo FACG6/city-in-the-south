18edce2ddc9b85b1bd3026492c858654491c8e52
Ahmed Shatat <a.shatat@hotmail.com>
2019-06-01 03:14:51 +0300

    feat: add slider
    #12

--- client/src/Components/LandingPage/index.jsx
+++ client/src/Components/LandingPage/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+
 import './style.css';
-import { Button, Alert } from 'react-bootstrap';
+
 import Slider from './Slider';
 
 export default function LandingPage() {
@@ -11,21 +12,6 @@ export default function LandingPage() {
           Want good (fair and meaningful) work or want to offer good work? Join
           us and find/show opportunities to connect and work together.
         </h2>
-      </div>
-      <div className="slider">
-        <Button variant="flat" size="xxl">
-          flat button
-        </Button>
-      </div>
-      <div className="about">
-        <Alert dismissible variant="danger">
-          <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
-          <p>
-            Change this and that and try again. Duis mollis, est non commodo
-            luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
-            Cras mattis consectetur purus sit amet fermentum.
-          </p>
-        </Alert>
       </div>
       <Slider />
     </>
