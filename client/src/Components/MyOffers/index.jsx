1192323d04a81669836b99d956a1607f898a1de8
Angham116 <angham.1161995@hotmail.com>
2019-06-02 09:56:05 +0300

    solve request change Relates #21

--- client/src/Components/MyOffers/index.jsx
+++ client/src/Components/MyOffers/index.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { Card, Row, Col, Container, Spinner } from 'react-bootstrap';
+
 import offersDetails from '../utilis/myOffers';
+
 import './style.css';
 
 class MyOffers extends Component {
@@ -18,16 +20,7 @@ class MyOffers extends Component {
   }
 
   statusClassName = status => {
-    switch (status) {
-      case 'completed':
-        return 'card-myoffers__status--completed';
-      case 'pending':
-        return 'card-myoffers__status--pending';
-      case 'finished':
-        return 'card-myoffers__status--finished';
-      default:
-        return 'card-myoffers__status--active';
-    }
+    return `myoffers__container__card__status--${status}`;
   };
 
   render() {
@@ -35,34 +28,32 @@ class MyOffers extends Component {
     // eslint-disable-next-line react/prop-types
     const { history } = this.props;
     return (
-      <Container className="page__container myoffers-container">
-        <div className="myOffers-head">
-          <h1 className="card-myoffers-head__title"> My Offers </h1>
-        </div>
+      <Container>
+        <h1 className="myoffers__container--title"> My Offers </h1>
         <Row>
           {offers ? (
             offers.map(item => {
               return (
                 <Col xs={12} md={6} lg={4} key={item.id}>
                   <Card
-                    className="card-myoffers"
+                    className="myoffers__container__card"
                     key={item.id}
                     onClick={() => history.push(`/app/offers/${item.id}`)}
                   >
-                    <Card.Header className="card-myoffers__header">
+                    <Card.Header className="myoffers__container__card--header">
                       <Card.Text
-                        className={`card-myoffers__status ${this.statusClassName(
+                        className={`myoffers__container__card__status ${this.statusClassName(
                           item.status
                         )}`}
                       >
                         {item.status}
                       </Card.Text>
                     </Card.Header>
-                    <Card.Body className="card-myoffers__body">
-                      <Card.Text className="card-myoffers__body--title">
+                    <Card.Body className="myoffers__container__card__body">
+                      <Card.Text className="myoffers__container__card__body--title">
                         {item.title}
                       </Card.Text>
-                      <Card.Text className="card-myoffers__body--position">
+                      <Card.Text className="myoffers__container__card__body--position">
                         {item.position}
                       </Card.Text>
                     </Card.Body>
