579a52fb169d5f4cfad1fa377a85bb94b82fa0a6
Fatma Siam <fatma.o.siam@gmail.com>
2019-06-02 10:11:27 +0300

    style: fixed AutoCompleteTags Component issues

--- client/src/Components/CommonComponents/AutoCompleteTags/index.jsx
+++ client/src/Components/CommonComponents/AutoCompleteTags/index.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Spinner } from 'react-bootstrap';
 import { Typeahead } from 'react-bootstrap-typeahead';
-import PropTypes from 'prop-types';
-import 'react-bootstrap-typeahead/css/Typeahead.css';
+
 import { skills, offerType } from './staticData';
 
+import 'react-bootstrap-typeahead/css/Typeahead.css';
 import './style.css';
 
 export default class AutoCompleteTags extends Component {
@@ -18,39 +19,45 @@ export default class AutoCompleteTags extends Component {
     // git the data form the back for skills and offer_type and setSatet for the skills and offer type
     // const selected = options.filter(item => _.find(fetched, id => id === item.id))
     const { data, type } = this.props;
-    this.setState({ selectedTags: data });
-
-    if (type === 'skill') {
-      this.setState({ options: skills });
-    }
-    if (type === 'offer_type') {
-      this.setState({ options: offerType });
-    }
+    this.setState(() => {
+      let options;
+      if (type === 'skill') {
+        options = skills;
+      } else if (type === 'offer_type') {
+        options = offerType;
+      }
+      return { selectedTags: data, options };
+    });
   }
 
   handleChange = items => {
     /* filter the skills if the user enter new skills we will make a request to the back to add the skills the user adding it 
     and the same as for the offer_type
   */
+    const { selectedTags } = this.state;
     const { onchange } = this.props;
-    this.setState(prevState => {
-      const newOptions = [...prevState.options];
-      const newTags = [...prevState.selectedTags];
-      if (items.length > newTags.length) {
-        if (items[items.length - 1].customOption) {
-          // send request to db to add skills/offertypes then return newOptions
-          // push the newOptions
-          newOptions.push({ id: 222, name: items[items.length - 1].name });
-          newTags.push({ id: 222, name: items[items.length - 1].name });
-        } else {
-          newTags.push(items[items.length - 1]);
+    this.setState(
+      prevState => {
+        const newOptions = [...prevState.options];
+        const newTags = [...prevState.selectedTags];
+        if (items.length > newTags.length) {
+          if (items[items.length - 1].customOption) {
+            // send request to db to add skills/offertypes then return newOptions
+            // push the newOptions
+            newOptions.push({ id: 222, name: items[items.length - 1].name });
+            newTags.push({ id: 222, name: items[items.length - 1].name });
+          } else {
+            newTags.push(items[items.length - 1]);
+          }
+          return { options: newOptions, selectedTags: newTags };
         }
-        return { options: newOptions, selectedTags: newTags };
+        return { selectedTags: items };
+      },
+      /* here we need another on change its comming from the filter it self */
+      () => {
+        if (typeof onchange === 'function') onchange(selectedTags);
       }
-      return { selectedTags: items };
-    });
-    /* here we need another on change its comming from the filter it self */
-    if (typeof onchange === 'function') onchange();
+    );
   };
 
   handleInputChange = item => {
@@ -67,7 +74,7 @@ export default class AutoCompleteTags extends Component {
     const { placeholder, type } = this.props;
     return (
       <>
-        <h1 className="autocomplete-title">
+        <h1 className="autocomplete__title">
           {type === 'skill' ? 'Skills ' : 'Offer Type'}
         </h1>
         {options[0] ? (
@@ -78,7 +85,6 @@ export default class AutoCompleteTags extends Component {
             onChange={this.handleChange}
             id={`autoComplete${type}`}
             key="id"
-            // defaultSelected={selectedTags}
             selected={selectedTags}
             valueKey="id"
             labelKey="name"
