117a98ef1ccd43dc45cb5d36ebe48291a7bec8c8
AymanAlQoqa <aalqouqa@gmail.com>
2019-06-02 22:23:44 +0300

    modify controller index file to include all other routes relates #82

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-  res.send('Server is running!!');
-});
+const login = require('./login');
+const logout = require('./logout');
+const members = require('./members');
+const offers = require('./offers');
+const savedOffers = require('./savedOffers');
+const applications = require('./applications');
+const offerApplications = require('./offerApplications');
+const myApplications = require('./myApplications');
+
+router.use('/login', login);
+router.get('/logout', logout);
+router.use('/members', members);
+router.use('/offers', offers);
+router.use('/saved-offers', savedOffers);
+router.use('/applications', applications);
+router.use('/offer-applications', offerApplications);
+router.use('/my-applications', myApplications);
 
 module.exports = router;
