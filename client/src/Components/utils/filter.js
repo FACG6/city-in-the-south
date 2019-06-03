cffa0a0b42cbdf478afd6df92d723cebe5550fb8
Fatma Siam <fatma.o.siam@gmail.com>
2019-06-03 09:13:40 +0300

    feat: add static data

---
+++ client/src/Components/utils/filter.js
@@ -0,0 +1,5 @@
+export default {
+  member_id: 10,
+  skills: [],
+  offer_type: [{ id: 1, name: 'Full Time' }, { id: 10, name: 'Fixed price' }],
+};
---
+++ client/src/Components/utils/skills.js
@@ -0,0 +1,7 @@
+export default [
+  { id: 1, name: 'React' },
+  { id: 2, name: 'Node Js' },
+  { id: 3, name: 'Express JS' },
+  { id: 4, name: 'React Native' },
+  { id: 5, name: 'Asp.net' },
+];
