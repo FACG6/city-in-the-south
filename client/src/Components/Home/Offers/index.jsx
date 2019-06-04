ab0bd50964b767df885bce708bbb5dff9533d204
Fatma Siam <fatma.o.siam@gmail.com>
2019-06-04 16:25:35 +0300

    feat: add Offers component
    separate the file

--- client/src/Components/Home/Offers/index.jsx
+++ client/src/Components/Home/Offers/index.jsx
@@ -1,6 +1,29 @@
 import React from 'react';
-import UnderConstruction from '../../UnderConstruction';
+import PropTypes from 'prop-types';
 
-export default function Offers() {
-  return <UnderConstruction />;
+import OfferCard from '../../CommonComponents/OfferCard';
+
+export default function Offers({ filtered }) {
+  return (
+    <div>
+      {filtered[0] ? (
+        filtered.map(item => {
+          if (item.status === 'active') {
+            return <OfferCard offer={item} key={item.id} hover={false} />;
+          }
+        })
+      ) : (
+        <div className="main-spinner"> There is no result</div>
+      )}
+    </div>
+  );
 }
+
+Offers.propTypes = {
+  filtered: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      ...PropTypes.string,
+    })
+  ).isRequired,
+};
