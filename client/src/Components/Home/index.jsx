497a68dabb9f36e8e13d628acb351a5591ba3c69
Fatma Siam <fatma.o.siam@gmail.com>
2019-06-04 16:26:17 +0300

    refactor : refactor home component

--- client/src/Components/Home/index.jsx
+++ client/src/Components/Home/index.jsx
@@ -6,15 +6,14 @@ import {
   Button,
   FormControl,
   Dropdown,
-  Spinner,
-  Card,
 } from 'react-bootstrap';
 
 import AutoCompleteTags from '../CommonComponents/AutoCompleteTags';
-import OfferCard from '../CommonComponents/OfferCard';
+import Offers from './Offers';
+import Members from './Members';
 
 import membetDetails from '../utils/members';
-import offersDetails from '../utils/offers';
+import offersDetails from '../utils/offers.1';
 import filter from '../utils/filter';
 import memberSkills from '../utils/skills';
 
@@ -25,18 +24,11 @@ export default class Home extends Component {
     offset: 0,
     isClicked: false,
     offers: [],
-    filteredOffers: [],
     isActive: '',
-    filteredMembers: [],
+    members: [],
     skills: [],
     offerType: [],
-    members: [],
   };
-
-  // get the filter from the back and passing it into the AutoComplete Component with props name --data--
-  // and here we need to get 100 records from the data to filtering it
-  /* and we need to check if the length of filter data less than 10 we should make a request to the back to get more data 
-  with offset +100 */
 
   componentDidMount() {
     if (filter.skills[0]) this.setState({ skills: filter.skills });
@@ -46,75 +38,51 @@ export default class Home extends Component {
     if (filter.offer_type[0]) this.setState({ offerType: filter.offer_type });
 
     this.setState({ offers: offersDetails });
-    this.setState({ filteredOffers: offersDetails });
-    this.setState({ isActive: 'Offers' });
-    this.setState({ offset: 100 });
-    this.setState({ filteredMembers: membetDetails });
+    this.setState({ isActive: localStorage.getItem('isActive') });
     this.setState({ members: membetDetails });
   }
 
   handleSkillOnChange = skills => {
-    // here we make a filter and check the length for both the members and the offers
-    // make a request to the filter with patch
-    console.log(111111111, skills);
-    const { isActive, members } = this.state;
-    let newfilteredMember;
-    this.setState({ filteredMembers: [] });
-    if (isActive === 'Members') {
-      console.log(newfilteredMember);
-      newfilteredMember = [];
-      newfilteredMember.length = 0;
-      members.filter(member => {
-        member.skill.filter(memberSkill => {
-          skills.filter(skill => {
-            if (memberSkill.name === skill.name) {
-              console.log(newfilteredMember);
-              newfilteredMember.push(member);
-              newfilteredMember.filter(newfilter => {
-                // if (newfilter.id !== member.id)
-              });
-            }
-          });
-        });
-        skills.filter(skill => console.log(skill));
-        this.setState({ filteredMembers: newfilteredMember });
-      });
+    const { isActive, members, offers } = this.state;
 
-      //   // console.log(skills);
-      //   // console.log(members);
-      // }
-      // if (isActive === 'Members') {
-      //   members.filter(member => {
-      //     console.log(member);
-      //     skills.map(memberskill => {
-      //       console.log(memberskill);
-      //       if (member.skills.name === memberskill.name) {
-      //         this.setState({ filteredMembers: member });
-      //       }
-      //     });
-      //   });
-      // }
+    if (isActive === 'Members') {
+      let memberArray = [];
+      this.setState({ members: [] });
+      memberArray = members.filter(
+        member =>
+          !skills.filter(
+            skill =>
+              !member.skill.filter(_skill => _skill.id === skill.id).length
+          ).length
+      );
+      this.setState({ members: memberArray });
+    }
+    if (isActive === 'Offers') {
+      let offerArray = [];
+      this.setState({ offers: [] });
+      offerArray = offers.filter(
+        offer =>
+          !skills.filter(
+            skill =>
+              !offer.skill.filter(_skill => _skill.id === skill.id).length
+          ).length
+      );
+      this.setState({ offers: offerArray });
     }
   };
 
-  handleOfferTypeOnChange = offertype => {
-    // here we make a filter and check the length for  the members
-    // make a request to the filter with patch
-    console.log('offertype', offertype);
-  };
+  handleOfferTypeOnChange = offertype => {};
 
   render() {
     const {
       isClicked,
       offers,
-      filteredOffers,
       isActive,
-      filteredMembers,
+      members,
       skills,
       offerType,
     } = this.state;
     // eslint-disable-next-line react/prop-types
-    const { history } = this.props;
     return (
       <>
         <Row className="home__contanier">
@@ -149,9 +117,7 @@ export default class Home extends Component {
                 </InputGroup>
               </Col>
               <Col className="home__result-label" xs={2}>
-                {isActive === 'Members'
-                  ? filteredMembers.length
-                  : filteredOffers.length}{' '}
+                {isActive === 'Members' ? members.length : offers.length}{' '}
                 results
               </Col>
               <Col className="dropdown-toggled" xs={3}>
@@ -159,16 +125,24 @@ export default class Home extends Component {
                   <Dropdown.Menu show className="dropdwon-list">
                     <Dropdown.Header
                       onClick={() => {
-                        this.setState({ isActive: 'Offers', isClicked: false });
+                        this.setState({
+                          isActive: 'Offers',
+                          isClicked: false,
+                        });
+                        localStorage.setItem('isActive', 'Offers');
                       }}
                     >
                       Offers
                     </Dropdown.Header>
                     <Dropdown.Item
                       eventKey="2"
-                      onClick={() =>
-                        this.setState({ isActive: 'Members', isClicked: false })
-                      }
+                      onClick={() => {
+                        this.setState({
+                          isActive: 'Members',
+                          isClicked: false,
+                        });
+                        localStorage.setItem('isActive', 'Members');
+                      }}
                     >
                       Members
                     </Dropdown.Item>
@@ -188,89 +162,10 @@ export default class Home extends Component {
               </Col>
             </Row>
             <hr className="hr-line" />
-
             {isActive === 'Offers' ? (
-              <div>
-                {offers[0] ? (
-                  offers.map(item => {
-                    if (item.status === 'active') {
-                      return (
-                        <OfferCard offer={item} key={item.id} hover={false} />
-                      );
-                    }
-                  })
-                ) : (
-                  <div className="main-spinner">
-                    {' '}
-                    <Spinner animation="border" variant="info" />
-                  </div>
-                )}{' '}
-              </div>
+              <Offers filtered={offers} />
             ) : (
-              <Row>
-                {filteredMembers[0] ? (
-                  filteredMembers.map(member => {
-                    return (
-                      <Col xs={12} md={4} lg={4} key={member.id}>
-                        <Card
-                          className="member-card"
-                          key={member.id}
-                          onClick={() =>
-                            history.push(`/profile/${member.username}`)
-                          }
-                        >
-                          <Card.Body>
-                            <Row>
-                              <Col xs={6} md={5}>
-                                <Card.Img
-                                  src={member.avatar}
-                                  className="member-card__avatar"
-                                />
-                              </Col>
-                              <Col
-                                xs={6}
-                                md={7}
-                                className="member-card__username"
-                              >
-                                <Card.Text>{member.username}</Card.Text>
-                              </Col>
-                            </Row>
-                            <br />
-                            <div>
-                              Skills :
-                              <br />
-                              <br />
-                              {member.skill[0] && (
-                                <>
-                                  {member.skill[0] && (
-                                    <span className="member-card__skill">
-                                      {member.skill[0].name}
-                                    </span>
-                                  )}
-                                  {member.skill[1] && (
-                                    <span className="member-card__skill">
-                                      {member.skill[1].name}
-                                    </span>
-                                  )}
-                                  {member.skill[2] && (
-                                    <span className="member-card__skill">
-                                      {member.skill[2].name}
-                                    </span>
-                                  )}
-                                </>
-                              )}
-                            </div>
-                          </Card.Body>
-                        </Card>
-                      </Col>
-                    );
-                  })
-                ) : (
-                  <div className="main-spinner">
-                    <Spinner animation="border" variant="info" />
-                  </div>
-                )}
-              </Row>
+              <Members filtered={members} {...this.props} />
             )}
           </Col>
         </Row>
