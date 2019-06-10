6d8ffa56f98b200ab22ba57ae8871b9ef2125415
alaabadra <alaabadra4@gmail.com>
2019-06-10 17:13:41 +0300

    editing in file getMyOffer in controller

--- server/controllers/offer/getMyOffers.js
+++ server/controllers/offer/getMyOffers.js
@@ -1,8 +1,10 @@
-condt {getMyOffer} = require('../../database/queries/offers/getMyOffer');
+const { getMyOffer } = require('../../database/queries/offers/getMyOffer');
 
 module.exports = (req, res) => {
-const {memberId} = req.params;
-get  
-  
-  res.send(' get ny offers ');
+  const { memberId } = req.params;
+  getMyOffer(memberId)
+    .then(result => res.send({
+      error: null,
+      data: result.rows,
+    }));
 };
