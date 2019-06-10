87c338e4b94e00e28ad5cafdfdd8663339d2ef54
AymanAlqoqa <31068752+AymanAlqoqa@users.noreply.github.com>
2019-06-10 09:55:42 +0300

    change Error text in db_connection

--- server/database/config/db_connection.js
+++ server/database/config/db_connection.js
@@ -7,7 +7,7 @@ switch (process.env.NODE_ENV) {
   case 'testing': connectionString = process.env.TESTING_DB; break;
   case 'production': connectionString = process.env.DATABASE_HEROCU; break;
   case 'development': connectionString = process.env.DATABASE_URL; break;
-  default: throw new Error('not found the DB_URL');
+  default: throw new Error('Database url is not found!!!');
 }
 
 module.exports = new Pool({
