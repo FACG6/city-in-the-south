8287baab05b030c9c78e8bb3dff58d2fbe256a01
AymanAlQoqa <aalqouqa@gmail.com>
2019-06-10 16:41:13 +0300

    modify getOfferApplication route to return offer applications to client relates #43

--- server/controllers/applications/getOfferApplication.js
+++ server/controllers/applications/getOfferApplication.js
@@ -1,3 +1,13 @@
-module.exports = (req, res) => {
-  res.send('get offer application');
+const getOfferApplications = require('../../database/queries/applications/getOfferApplications');
+
+module.exports = (req, res, next) => {
+  const { offerId } = req.params;
+  getOfferApplications(offerId)
+    .then((result) => {
+      res.send({
+        error: null,
+        data: result.rows,
+      });
+    })
+    .catch(() => next({ code: 500, msg: 'Internal Server Error!' }));
 };
