9661ac2206ace90c721b15ed98c6f1bcbf9f82fa
Fatma Siam <fatma.o.siam@gmail.com>
2019-06-10 20:47:56 +0300

    refactor: edit static filter data

--- client/src/Components/utils/filter.js
+++ client/src/Components/utils/filter.js
@@ -1,5 +1,5 @@
 export default {
   member_id: 10,
   skills: [],
-  offer_type: [{ id: 1, name: 'Full Time' }, { id: 10, name: 'Fixed price' }],
+  offer_type: [{ id: 1, name: 'Full time' }, { id: 2, name: 'fixed price' }],
 };
