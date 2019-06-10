6722623db5d5ab9df6bc06a5ff9f098cd80754d8
Fatma Siam <fatma.o.siam@gmail.com>
2019-06-10 14:35:45 +0300

    feat: add saved-offers controlers Relates #55

--- server/controllers/offer/addSavedOffer.js
+++ server/controllers/offer/addSavedOffer.js
@@ -1,3 +1,11 @@
-module.exports = (req, res) => {
-  res.send('add saved offer ');
+const { addSavedOffers } = require('../../database/queries/offers/saved-offers');
+
+module.exports = (req, res, next) => {
+  const savedOfferInfo = req.body;
+  addSavedOffers(savedOfferInfo)
+    .then(({ rows: saved }) => {
+      if (!saved[0]) return next({ code: 400, msg: 'Bad Request ' });
+      return res.status(200).send({ error: null, data: saved[0] });
+    })
+    .catch(err => next(err));
 };
