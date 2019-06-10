5f27f4fcf8192cd5b96228d2e86ffecd73ae734d
Fatma Siam <fatma.o.siam@gmail.com>
2019-06-10 15:45:55 +0300

    feat: add error middleware Relates: #91

--- server/controllers/errors/index.js
+++ server/controllers/errors/index.js
@@ -1,9 +1,19 @@
+const createError = require('http-errors');
+
 exports.notFound = (req, res) => {
-  res.send('not found');
+  res.status(404).send(createError(404, 'Page Not Found ...!'));
 };
 
-
 exports.serverError = (err, req, res, next) => {
-  console.log(err);
-  res.send('Error 500');
+  switch (err.code) {
+    case 401:
+      res.status(400).send(createError(400, err.msg));
+      break;
+    case 403:
+      res.status(403).send(createError(403, 'unauthorization '));
+      break;
+    default:
+      res.status(500).send(createError(500, 'Internal Server Error'));
+      break;
+  }
 };
