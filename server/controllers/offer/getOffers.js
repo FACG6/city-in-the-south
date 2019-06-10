54d115ac08d1c4140c5fa20c99f45c7b2bdbbf7e
Angham116 <angham.1161995@hotmail.com>
2019-06-10 14:37:47 +0300

    getOffers route Relates #58

--- server/controllers/offer/getOffers.js
+++ server/controllers/offer/getOffers.js
@@ -1,3 +1,10 @@
-module.exports = (req, res) => {
-  res.send('get offers  ');
+const { getOffers } = require('../../database/queries/offers/index');
+
+module.exports = (req, res, next) => {
+  getOffers()
+    .then(response => res.send({
+      error: null,
+      data: response.rows,
+    }))
+    .catch(err => next(err));
 };
