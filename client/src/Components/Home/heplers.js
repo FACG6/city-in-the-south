9f11eecacaddd2ba41ac8eb737ecc6ccde8d984a
Fatma Siam <fatma.o.siam@gmail.com>
2019-06-10 20:46:53 +0300

    refactor: refactor filterData function

--- client/src/Components/Home/heplers.js
+++ client/src/Components/Home/heplers.js
@@ -1,8 +1,22 @@
-const filterData = (arr, filterQuery) => {
+const filterData = (arr, skillsQuery, offerTypeQuery) => {
   return arr.filter(
     item =>
-      !filterQuery.filter(
-        query => !item.skill.filter(_query => _query.id === query.id).length
+      !skillsQuery.filter(
+        query =>
+          !item.skill.filter(_query => {
+            if (offerTypeQuery[0]) {
+              return (
+                _query.id === query.id &&
+                !offerTypeQuery.filter(
+                  offerquery =>
+                    !item.offer_type.filter(
+                      _offertype => _offertype.id === offerquery.id
+                    ).length
+                ).length
+              );
+            }
+            return _query.id === query.id;
+          }).length
       ).length
   );
 };
