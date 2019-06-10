2b0bb80bd3dc4aecadd31bc98b95e6e9b8265603
AymanAlQoqa <aalqouqa@gmail.com>
2019-06-10 16:41:57 +0300

    add getOfferApplications testing relates #43

---
+++ test/getOfferApplications.js
@@ -0,0 +1,27 @@
+const test = require('tape');
+const supertest = require('supertest');
+const app = require('../server/app');
+
+// getOfferApplications
+test('test getOfferApplications', (t) => {
+  supertest(app)
+    .get('/api/v1//offer-applications/1')
+    .expect(200)
+    .expect('content-type', /json/)
+    .end((err, response) => {
+      if (err) {
+        t.error(err);
+      }
+      t.deepEqual(
+        Object.keys(response.body),
+        ['error', 'data'],
+        'Should return object of two keys (error and data)',
+      );
+      t.deepEqual(
+        Object.keys(response.body.data[0]),
+        ['member_id', 'username', 'full_name', 'avatar', 'proposal', 'status'],
+        'data should be array of objects each object contains 6 keys',
+      );
+      t.end();
+    });
+});
