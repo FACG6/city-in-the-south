bc23a1608fd7a352178e6e89cb3780f36c69a94b
Angham116 <angham.1161995@hotmail.com>
2019-06-10 23:39:38 +0300

    query function for get offers Relates #58

--- server/controllers/offer/getOffers.js
+++ server/controllers/offer/getOffers.js
@@ -2,9 +2,12 @@ const { getOffers } = require('../../database/queries/offers/index');
 
 module.exports = (req, res, next) => {
   getOffers()
-    .then(response => res.send({
-      error: null,
-      data: response.rows,
-    }))
+    .then((response) => {
+      console.log(11111, response.rows);
+      res.send({
+        error: null,
+        data: response.rows,
+      });
+    })
     .catch(err => next(err));
 };
