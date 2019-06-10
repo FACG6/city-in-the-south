8b927739daa76b0759fd7d45dce19acbb8b2bed5
alaabadra <alaabadra4@gmail.com>
2019-06-10 16:58:53 +0300

    create getMyOffer

--- server/controllers/offer/getMyOffers.js
+++ server/controllers/offer/getMyOffers.js
@@ -1,3 +1,8 @@
+condt {getMyOffer} = require('../../database/queries/offers/getMyOffer');
+
 module.exports = (req, res) => {
+const {memberId} = req.params;
+get  
+  
   res.send(' get ny offers ');
 };
