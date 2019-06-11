dfd2d4d9aee809246f45343b9b55025b4339c8e2
Angham116 <angham.1161995@hotmail.com>
2019-06-11 11:00:08 +0300

    style: remove console.log Relates #58

--- server/controllers/offer/getOffers.js
+++ server/controllers/offer/getOffers.js
@@ -3,7 +3,6 @@ const { getOffers } = require('../../database/queries/offers/index');
 module.exports = (req, res, next) => {
   getOffers()
     .then((response) => {
-      console.log(11111, response.rows);
       res.send({
         error: null,
         data: response.rows,
