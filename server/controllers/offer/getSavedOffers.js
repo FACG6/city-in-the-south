5e2ca69218d1906b9e4eca6950bdde63adbbf551
alaabadra <alaabadra4@gmail.com>
2019-06-11 09:40:20 +0300

    add catch error 500 in file getSavedOffers

--- server/controllers/offer/getSavedOffers.js
+++ server/controllers/offer/getSavedOffers.js
@@ -1,10 +1,10 @@
 const { getSavedOffer } = require('../../database/queries/offers/getSavedOffer.js');
 
-module.exports = (req, res) => {
+module.exports = (req, res, next) => {
   const { memberId } = req.params;
   getSavedOffer(memberId)
     .then(result => res.send({
       error: null,
       data: result.rows,
-    }));
+    })).catch(() => next({ code: 500, msg: 'internal server error' }));
 };
