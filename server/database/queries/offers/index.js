f3b10fa8fb43124ba6af547d276fa5f25bca835a
Angham116 <angham.1161995@hotmail.com>
2019-06-11 11:13:57 +0300

    feat: make the getOffers query with limitation Relates #58

--- server/database/queries/offers/index.js
+++ server/database/queries/offers/index.js
@@ -1,8 +1,9 @@
 const connection = require('../../config/db_connection');
 
-exports.getOffers = () => connection.query(`select offer.* , COALESCE(json_agg(skill) filter (where skill.id is not null),'[]') as skills, COALESCE(json_agg(offer_type) filter (where skill.id is not null),'[]') as offer_types from offer
+exports.getOffers = offset => connection.query(`select offer.* , COALESCE(json_agg(skill) filter (where skill.id is not null),'[]') as skills,
+COALESCE(json_agg(offer_type) filter (where skill.id is not null),'[]') as offer_types from offer
 left outer join offer_skill ON offer_skill.offer_id = offer.id
 left outer JOIN skill ON skill.id = offer_skill.skill_id
 left outer join offer_offer_type ON offer_offer_type.offer_id = offer.id
 LEFT outer JOIN offer_type ON offer_type.id = offer_offer_type.offer_type_id
-GROUP BY offer.id`);
+GROUP BY offer.id LIMIT 100 OFFSET $1`, [offset]);
