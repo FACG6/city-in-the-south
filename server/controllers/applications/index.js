2fff7e6191340101e23f2f0ec3f7d4f037363568
AymanAlQoqa <aalqouqa@gmail.com>
2019-06-11 09:51:29 +0300

    modify index to add getMyApplications and getMyApllication routes

--- server/controllers/applications/index.js
+++ server/controllers/applications/index.js
@@ -2,8 +2,14 @@ const addHireMember = require('./addHiredMember');
 const updateHireMember = require('./patchHiredMember');
 const addApplication = require('./addApplication');
 const getOfferApplication = require('./getOfferApplication');
+const getMyApplications = require('./getMyApplications');
 const getMyApplication = require('./getMyApplication');
 
 module.exports = {
-  addHireMember, updateHireMember, addApplication, getOfferApplication, getMyApplication,
+  addHireMember,
+  updateHireMember,
+  addApplication,
+  getOfferApplication,
+  getMyApplication,
+  getMyApplications,
 };
