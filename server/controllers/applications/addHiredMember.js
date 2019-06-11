9881285b596dce75a9331a760682ec806f9af616
AymanAlQoqa <aalqouqa@gmail.com>
2019-06-11 12:47:37 +0300

    add addHiredMember cotroller route to handle request and add data to database relates #56

--- server/controllers/applications/addHiredMember.js
+++ server/controllers/applications/addHiredMember.js
@@ -1,3 +1,26 @@
-module.exports = (req, res) => {
-  res.send('new hire member');
+const yup = require('yup');
+const { addHiredMember } = require('../../database/queries/applications/index');
+
+module.exports = (req, res, next) => {
+  const { member_id: memberId, offer_id: offerId, status } = req.body;
+  const schema = yup.object({
+    offerId: yup.string().required(),
+    memberId: yup.string().required(),
+    status: yup.string().required(),
+  });
+  schema
+    .validate({ offerId, memberId, status })
+    .then(() => {
+      addHiredMember(memberId, offerId, status)
+        .then(() => {
+          res.send({
+            error: null,
+            data: [req.body],
+          });
+        })
+        .catch(err => next({ code: 500, msg: err.message }));
+    })
+    .catch((err) => {
+      next({ code: 400, msg: err.message });
+    });
 };
