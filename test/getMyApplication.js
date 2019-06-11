9bd79f4b430d00192f190d06ec8e32082e70b732
AymanAlQoqa <aalqouqa@gmail.com>
2019-06-11 10:48:40 +0300

    add test for getMyApplication relates #41

--- test/getMyApplication.js
+++ test/getMyApplication.js
@@ -5,30 +5,9 @@ const app = require('../server/app');
 // getMyApplication for specific offerId
 test('test getMyApplication', (t) => {
   supertest(app)
-    .get('/api/v1/my-applications/2?offerId=1')
+    .get('/api/v1/2/my-applications/1')
     .expect(200)
-    .end((err, response) => {
-      if (err) {
-        t.error(err);
-      }
-      t.deepEqual(
-        Object.keys(response.body),
-        ['error', 'data'],
-        'Should object of two keys (error and data)',
-      );
-      t.deepEqual(
-        Object.keys(response.body.data),
-        ['member_id', 'username', 'full_name', 'avatar', 'proposal', 'status'],
-        'Should object of 6 keys',
-      );
-      t.end();
-    });
-});
-// getMyApplications
-test('test getMyApplications', (t) => {
-  supertest(app)
-    .get('/api/v1/my-applications/2')
-    .expect(200)
+    .expect('content-type', /json/)
     .end((err, response) => {
       if (err) {
         t.error(err);
@@ -40,8 +19,8 @@ test('test getMyApplications', (t) => {
       );
       t.deepEqual(
         Object.keys(response.body.data[0]),
-        ['member_id', 'offer_id', 'username', 'full_name', 'avatar', 'proposal', 'status'],
-        'data should be array of objects each object contains 6 keys',
+        ['member_id', 'username', 'full_name', 'avatar', 'proposal', 'status'],
+        'Should object of 6 keys',
       );
       t.end();
     });
