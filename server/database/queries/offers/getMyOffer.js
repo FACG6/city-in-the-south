de580819002ddf1c18ce6bd838393eb844caeb55
Fatma Siam <fatma.o.siam@gmail.com>
2019-06-11 17:45:20 +0300

    refactor: refactor getMyOffer query

--- server/database/queries/offers/getMyOffer.js
+++ server/database/queries/offers/getMyOffer.js
@@ -1,4 +1,4 @@
 const dbconnection = require('../../config/db_connection.js');
 
-const getMyOffer = memberId => dbconnection.query('SELECT offer.status,offer.position,offer.title,offer.description,offer.member_id FROM offer WHERE offer.member_id = $1 ', [memberId]);
+const getMyOffer = memberId => dbconnection.query('SELECT * FROM offer WHERE offer.member_id = $1 ', [memberId]);
 module.exports = { getMyOffer };
