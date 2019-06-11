5347810cb8df1232d79bf275367163e3a37cee4c
Angham116 <angham.1161995@hotmail.com>
2019-06-11 13:41:05 +0300

    feat: route for add new skill Relates #48

--- server/controllers/skills/addSkills.js
+++ server/controllers/skills/addSkills.js
@@ -1,3 +1,16 @@
-module.exports = (req, res) => {
-  res.send(' add new skills ');
+const { checkSkills, insertSkills } = require('../../database/queries/skills/index');
+
+module.exports = (req, res, next) => {
+  const { name } = req.body;
+  checkSkills(name)
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return insertSkills(name);
+      } return next({ code: 400, msg: 'skill already exist' });
+    })
+    .then(response => res.send({
+      error: null,
+      data: response.rows[0],
+    }))
+    .catch(err => next(err));
 };
