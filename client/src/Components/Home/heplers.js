ec4c9361679e233bc81c4463371b5cc4754f0904
Fatma Siam <fatma.o.siam@gmail.com>
2019-06-11 09:23:17 +0300

    refactor:refactor the functions

--- client/src/Components/Home/heplers.js
+++ client/src/Components/Home/heplers.js
@@ -1,24 +1,26 @@
-const filterData = (arr, skillsQuery, offerTypeQuery) => {
+const filterSkills = (arr, skillQuery) => {
   return arr.filter(
     item =>
-      !skillsQuery.filter(
-        query =>
-          !item.skill.filter(_query => {
-            if (offerTypeQuery[0]) {
-              return (
-                _query.id === query.id &&
-                !offerTypeQuery.filter(
-                  offerquery =>
-                    !item.offer_type.filter(
-                      _offertype => _offertype.id === offerquery.id
-                    ).length
-                ).length
-              );
-            }
-            return _query.id === query.id;
-          }).length
+      !skillQuery.filter(
+        query => !item.skill.filter(_query => _query.id === query.id).length
       ).length
   );
 };
 
-module.exports = { filterData };
+const filterOfferTypes = (arr, offerTypeQuery) => {
+  return arr.filter(
+    item =>
+      !offerTypeQuery.filter(
+        query =>
+          !item.offer_type.filter(_query => _query.id === query.id).length
+      ).length
+  );
+};
+
+const filteredOffers = (value1, value2) => {
+  value1.filter(item => {
+    return value2.filter(_item => item.id === _item.id);
+  });
+};
+
+module.exports = { filterSkills, filterOfferTypes, filteredOffers };
