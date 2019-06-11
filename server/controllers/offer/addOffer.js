108bfa24a9aa87d1cfec2843cfaf16d9801dfd19
Ahmed <ahmedalmi95@gmail.com>
2019-06-11 16:44:06 +0300

    made offer post

--- server/controllers/offer/addOffer.js
+++ server/controllers/offer/addOffer.js
@@ -1,3 +1,17 @@
-module.exports = (req, res) => {
-  res.send(' add new offer');
+const { addOffer, addOfferType, addSkills, addOffersType } = require('./../../database/queries/offers/index');
+
+
+module.exports = async (req, res) => {
+  const { newSkills, newOffer_type, skills, offer_type, ...offer } = req.body;
+  const skillsId = skills.map(skill => skill.id);
+  const offer_typeIds = offer_type.map(type => type.id);
+
+  const newSkillsId = await addSkills(newSkills);
+  const allSkillsId = [...skillsId, ...newSkillsId];
+  const newoffer_typeId = await addOffersType(newOffer_type);
+  const offerId = (await addOffer(offer)).rows[0].id;
+  console.log('Offer ID  : ', offerId);
+  console.log('new offers ID  : ', [...newoffer_typeId, ...offer_typeIds]);
+  console.log('all skills  ID  : ', allSkillsId);
+  
 };
