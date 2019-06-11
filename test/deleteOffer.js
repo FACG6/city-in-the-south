ae52c3a0e06f259c5d3f6e5306783b2a7dd22fad
Angham116 <angham.1161995@hotmail.com>
2019-06-11 15:15:41 +0300

    test: testing for delete offer route Relates #53

--- test/deleteOffer.js
+++ test/deleteOffer.js
@@ -6,6 +6,27 @@ const router = require('../server/app');
 
 const insertOffer = () => connection.query('INSERT INTO offer(title, position, member_id) VALUES (\'small shop\', \'accountant\', 2)');
 
-const selectOffer = () => connection/query('SELECT * FROM offer LIMIT 1');
+const selectOffer = () => connection.query('SELECT * FROM offer LIMIT 1');
 
-// const deleteOffer = (id) => connection.query('')
+tape('Testing for DELETE : /api/v1/offers/:offerId', (t) => {
+  insertOffer();
+  let offerId;
+  selectOffer()
+    .then((res) => {
+      offerId = res.rows[0].id;
+      supertest(router)
+        .delete(`/api/v1/offers/${offerId}`)
+        .expect(200)
+        .expect('content-type', /json/)
+        .end((err, response) => {
+          if (err) t.error(err);
+          t.equal(response.body.data, 'success', 'Should return sucess message');
+          t.end();
+        });
+    })
+    .catch(err => console.log(err));
+});
+
+tape.onFinish(() => {
+  process.exit(0);
+});
