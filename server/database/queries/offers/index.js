85cb207cdd6d86265e050124796f7a4b753f17ae
Angham116 <angham.1161995@hotmail.com>
2019-06-11 09:43:16 +0300

    query function for get offers Relates #58

--- server/database/queries/offers/index.js
+++ server/database/queries/offers/index.js
@@ -1,9 +1,8 @@
 const connection = require('../../config/db_connection');
 
-exports.getOffers = () => connection.query(`select offer.* , json_agg(skill) as skills, json_agg(offer_type) as offer_types from offer
+exports.getOffers = () => connection.query(`select offer.* , COALESCE(json_agg(skill) filter (where skill.id is not null),'[]') as skills, COALESCE(json_agg(offer_type) filter (where skill.id is not null),'[]') as offer_types from offer
 left outer join offer_skill ON offer_skill.offer_id = offer.id
 left outer JOIN skill ON skill.id = offer_skill.skill_id
 left outer join offer_offer_type ON offer_offer_type.offer_id = offer.id
 LEFT outer JOIN offer_type ON offer_type.id = offer_offer_type.offer_type_id
-where offer_skill.skill_id is not null
 GROUP BY offer.id`);
