7cbd525be2a8e05a92d610b9c5e721296f453313
Angham116 <angham.1161995@hotmail.com>
2019-06-11 17:04:56 +0300

    style: typo

--- server/database/queries/offers/index.js
+++ server/database/queries/offers/index.js
@@ -1,9 +1,9 @@
 const connection = require('../../config/db_connection');
 
-exports.getOffers = offset => connection.query(`select offer.* , COALESCE(json_agg(skill) filter (where skill.id is not null),'[]') as skills,
-COALESCE(json_agg(offer_type) filter (where skill.id is not null),'[]') as offer_types from offer
-left outer join offer_skill ON offer_skill.offer_id = offer.id
-left outer JOIN skill ON skill.id = offer_skill.skill_id
-left outer join offer_offer_type ON offer_offer_type.offer_id = offer.id
-LEFT outer JOIN offer_type ON offer_type.id = offer_offer_type.offer_type_id
+exports.getOffers = offset => connection.query(`SELECT offer.* , COALESCE(json_agg(skill) FILTER (WHERE skill.id IS NOT NULL),'[]') AS skills,
+COALESCE(json_agg(offer_type) FILTER (WHERE skill.id IS NOT NULL),'[]') AS offer_types FROM offer
+LEFT OUTER JOIN offer_skill ON offer_skill.offer_id = offer.id
+LEFT OUTER JOIN skill ON skill.id = offer_skill.skill_id
+LEFT OUTER JOIN offer_offer_type ON offer_offer_type.offer_id = offer.id
+LEFT OUTER JOIN offer_type ON offer_type.id = offer_offer_type.offer_type_id
 GROUP BY offer.id LIMIT 100 OFFSET $1`, [offset]);
