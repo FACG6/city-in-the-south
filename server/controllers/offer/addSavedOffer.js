ade20e7b160086b5a7a539813668f6d5c282fb1d
Fatma Siam <fatma.o.siam@gmail.com>
2019-06-11 14:04:08 +0300

    refactor : refactor codes

--- server/controllers/offer/addSavedOffer.js
+++ server/controllers/offer/addSavedOffer.js
@@ -9,10 +9,7 @@ module.exports = (req, res, next) => {
     .then((valid) => {
       if (!valid) return next({ code: 400, msg: 'Ensure to enter validly data' });
       return addSavedOffers(savedOfferInfo)
-        .then(({ rows: saved }) => {
-          if (!saved[0]) return next({ code: 400, msg: 'Bad Request ' });
-          return res.status(200).send({ error: null, data: saved[0] });
-        })
+        .then(({ rows: savedOffers }) => res.status(200).send({ error: null, data: savedOffers }))
         .catch(err => next(err));
     })
     .catch(err => next(err));
