3c6cecb65f804d6f26f996af1cae2c6178a13ad4
AymanAlQoqa <aalqouqa@gmail.com>
2019-06-11 10:51:59 +0300

    modify test index to include getMyApplication/getMyApplications tests relates #41

--- test/index.js
+++ test/index.js
@@ -3,4 +3,5 @@ const { dbBuild, dbFakeData } = require('../server/database/config/db_build');
 dbBuild()
   .then(dbFakeData)
   .then(() => require('./getMyApplication'))
+  .then(() => require('./getMyApplications'))
   .catch(err => console.log(err));
