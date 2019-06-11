2cce359953de5ab31a358e97bff5dd487ee6493f
AymanAlQoqa <aalqouqa@gmail.com>
2019-06-11 10:47:17 +0300

    add getMyApplication to return member application for specific offer relates #41

--- server/controllers/applications/getMyApplication.js
+++ server/controllers/applications/getMyApplication.js
@@ -1,14 +1,12 @@
 const yup = require('yup');
 
-const getMyApplications = require('../../database/queries/applications/getMyApplications');
 const getMyApplication = require('../../database/queries/applications/getMyApplication');
 
 module.exports = (req, res, next) => {
-  const { offerId } = req.query;
-  const { memberId } = req.params;
+  const { memberId, offerId } = req.params;
 
   const schema = yup.object({
-    offerId: yup.string().nullable(),
+    offerId: yup.string().required(),
     memberId: yup.string().required(),
   });
 
@@ -18,25 +16,14 @@ module.exports = (req, res, next) => {
       memberId,
     })
     .then(() => {
-      if (offerId) {
-        getMyApplication(memberId, offerId)
-          .then((result) => {
-            res.send({
-              error: null,
-              data: result.rows[0],
-            });
-          })
-          .catch(() => next({ code: 500, msg: 'Internal Server Error' }));
-      } else {
-        getMyApplications(memberId)
-          .then((result) => {
-            res.send({
-              error: null,
-              data: result.rows,
-            });
-          })
-          .catch(() => next({ code: 500, msg: 'Internal Server Error' }));
-      }
+      getMyApplication(memberId, offerId)
+        .then((result) => {
+          res.send({
+            error: null,
+            data: result.rows,
+          });
+        })
+        .catch(() => next({ code: 500, msg: 'Internal Server Error' }));
     })
     .catch((err) => {
       next({ code: 400, msg: err.message });
