f6cb00950cf89f998b111b654f83554b9680d2a8
Fatma Siam <fatma.o.siam@gmail.com>
2019-06-11 12:05:21 +0300

    refactor: edit addsavedoffer route Relates: #55

--- server/controllers/offer/addSavedOffer.js
+++ server/controllers/offer/addSavedOffer.js
@@ -1,11 +1,19 @@
+const { SavedOfferSchema } = require('../../helpers/validation-schema');
+
 const { addSavedOffers } = require('../../database/queries/offers/saved-offers');
+
 
 module.exports = (req, res, next) => {
   const savedOfferInfo = req.body;
-  addSavedOffers(savedOfferInfo)
-    .then(({ rows: saved }) => {
-      if (!saved[0]) return next({ code: 400, msg: 'Bad Request ' });
-      return res.status(200).send({ error: null, data: saved[0] });
+  SavedOfferSchema.isValid(savedOfferInfo)
+    .then((valid) => {
+      if (!valid) return next({ code: 400, msg: 'Ensure to enter validly data' });
+      return addSavedOffers(savedOfferInfo)
+        .then(({ rows: saved }) => {
+          if (!saved[0]) return next({ code: 400, msg: 'Bad Request ' });
+          return res.status(200).send({ error: null, data: saved[0] });
+        })
+        .catch(err => next(err));
     })
     .catch(err => next(err));
 };
