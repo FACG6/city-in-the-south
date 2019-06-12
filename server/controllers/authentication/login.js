146ac9515404b9edd2c8677425bfae636cd2dc3c
Fatma Siam <fatma.o.siam@gmail.com>
2019-06-12 10:31:05 +0300

    refactor: refactor the login route

--- server/controllers/authentication/login.js
+++ server/controllers/authentication/login.js
@@ -19,9 +19,9 @@ module.exports = (req, res, next) => {
                 const token = sign(payload, process.env.SECRET);
                 res.cookie('jwt', token, { maxAge: 1000 * 60 * 60 * 24 * 1 }, { httpOnly: true });
                 res.status(200).send({ error: null, data: payload });
-              } else next({ code: 400, msg: 'Check your password' });
+              } else next({ code: 400, msg: 'Check your password ' });
             });
-          } else next({ code: 400, msg: 'Username does not exist' });
+          } else next({ code: 400, msg: 'Username does not exist ' });
         })
         .catch(err => next(err));
     })
