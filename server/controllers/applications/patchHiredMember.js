95f6badc9f9a5c5ff93ca86d6530d18de6043256
AymanAlQoqa <aalqouqa@gmail.com>
2019-06-12 13:33:59 +0300

    modify patchHiredMember to accept data from client and update status then return data back to client relates #57

--- server/controllers/applications/patchHiredMember.js
+++ server/controllers/applications/patchHiredMember.js
@@ -1,3 +1,37 @@
-module.exports = (req, res) => {
-  res.send('update hire member status');
+const yup = require('yup');
+const { patchHiredMember } = require('../../database/queries/applications/index');
+const updateOfferStatus = require('../../database/queries/offers/updateOfferStatus');
+
+module.exports = (req, res, next) => {
+  const { member_id: memberId, offer_id: offerId, status } = req.body;
+  const schema = yup.object({
+    offerId: yup.string().required(),
+    memberId: yup.string().required(),
+    status: yup.string().required(),
+  });
+  schema
+    .validate({ offerId, memberId, status })
+    .then(() => {
+      patchHiredMember(memberId, offerId, status)
+        .then((result) => {
+          if (result.rowCount && result.rows[0].status === 'accepted') {
+            // update offer status if Hired_member status = accepted
+            updateOfferStatus(offerId, 'completed')
+              .then(() => res.send({
+                error: null,
+                data: result.rows,
+              }))
+              .catch(err => next({ code: 500, msg: err.message }));
+          } else {
+            res.send({
+              error: null,
+              data: result.rows,
+            });
+          }
+        })
+        .catch(err => next({ code: 500, msg: err.message }));
+    })
+    .catch((err) => {
+      next({ code: 400, msg: err.message });
+    });
 };
