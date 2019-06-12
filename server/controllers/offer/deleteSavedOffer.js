ac4d1959ea349e722599292948ee45ddb7b3aa84
Fatma Siam <fatma.o.siam@gmail.com>
2019-06-12 14:49:51 +0300

    add delete saved-offer controller Relates : #60

--- server/controllers/offer/deleteSavedOffer.js
+++ server/controllers/offer/deleteSavedOffer.js
@@ -1,3 +1,12 @@
-module.exports = (req, res) => {
-  res.send('delete saved offer ');
+const { deleteSavedOffer } = require('../../database/queries/offers/deleteSavedOffer');
+
+module.exports = (req, res, next) => {
+  const { memberId } = req.params;
+  const { offerId } = req.body;
+  deleteSavedOffer({ memberId, offerId })
+    .then(({ rows: deletedSaved }) => {
+      if (!deletedSaved[0]) return next({ code: 400, msg: 'No record to delete ' });
+      return res.status(200).send({ error: null, data: 'success' });
+    })
+    .catch(err => next(err));
 };
