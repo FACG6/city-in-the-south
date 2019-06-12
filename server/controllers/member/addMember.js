b3805ec9d5a598d9911b80b09b9ea9962a76b789
Fatma Siam <fatma.o.siam@gmail.com>
2019-06-12 14:06:18 +0300

    refactor the addmember controller #35

--- server/controllers/member/addMember.js
+++ server/controllers/member/addMember.js
@@ -26,7 +26,7 @@ module.exports = (req, res, next) => {
           };
           const jwt = sign(payLoad, process.env.SECRET);
           res.cookie('jwt', jwt, { maxAge: 7200000 });
-          res.status(200).send({ error: null, data: payLoad });
+          res.status(200).send({ error: null, data: [payLoad] });
         })
         .catch(err => next(err));
     })
