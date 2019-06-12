4aa99f5fdbeb405dd67362967030ffa7110af261
Fatma Siam <fatma.o.siam@gmail.com>
2019-06-12 13:49:26 +0300

    feat: make add member controller Relates : #35

--- server/controllers/member/addMember.js
+++ server/controllers/member/addMember.js
@@ -1,3 +1,37 @@
-module.exports = (req, res) => {
-  res.send('signup -- add new member');
+const { sign } = require('jsonwebtoken');
+
+const { addMmeber } = require('../../database/queries/members/addMember');
+const { checkEmail } = require('../../database/queries/authentication/checkEmail');
+const { checkUsername } = require('../../database/queries/authentication/loginqueries');
+const { singUpSchema } = require('../../helpers/validation-schema');
+const { hashingPass } = require('../../helpers/hashPassword');
+
+module.exports = (req, res, next) => {
+  const memberInfo = { ...req.body };
+  singUpSchema.validate(memberInfo)
+    .then(() => {
+      checkUsername(memberInfo.username)
+        .then(({ rows: validUsername }) => {
+          if (validUsername[0]) throw next({ code: 400, msg: 'The username already exists ' });
+          else return checkEmail(memberInfo.email);
+        })
+        .then(({ rows: validEmail }) => {
+          if (validEmail[0]) throw next({ code: 400, msg: 'The email already exists ' });
+          else return hashingPass(memberInfo.pass);
+        })
+        .then(hashedPass => addMmeber({ ...memberInfo, pass: hashedPass }))
+        .then(({ rows: member }) => {
+          if (!member[0]) throw next({ code: 400, msg: 'Bad Request' });
+          else {
+            const payLoad = {
+              id: member[0].id, username: member[0].username, avatar: member[0].avatar,
+            };
+            const jwt = sign(payLoad, process.env.SECRET);
+            res.cookie('jwt', jwt, { maxAge: 7200000 });
+            res.status(200).send({ error: null, data: payLoad });
+          }
+        })
+        .catch(err => next(err));
+    })
+    .catch(err => next({ code: 400, msg: 'Ensure you enter validly data ' }));
 };
