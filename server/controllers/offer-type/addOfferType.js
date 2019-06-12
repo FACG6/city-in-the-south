3863c05274129e9402e54da8e4168f66ad815d9a
alaabadra <alaabadra4@gmail.com>
2019-06-12 13:36:02 +0300

    edit in file addOfferType in controller and add validation

--- server/controllers/offer-type/addOfferType.js
+++ server/controllers/offer-type/addOfferType.js
@@ -1,14 +1,19 @@
 const { addOfferType } = require('../../database/queries/offer-type/postOfferType.js');
+const yup = require('yup');
 
 module.exports = (req, res, next) => {
   const name = req.body;
-  console.log(name);
-
-  addOfferType(name)
-    .then((result) => {
-      res.send({
-        error: null,
-        data: result.rows[0],
-      });
-    }).catch(() => next({ code: 500, msg: 'internal server error' }));
+  const schema = yup.object({
+    name: yup.string().required(),
+  });
+  schema.validate({ name })
+    .then(() => {
+      addOfferType(name)
+        .then((result) => {
+          res.send({
+            error: null,
+            data: result.rows[0],
+          });
+        }).catch(() => next({ code: 500, msg: 'internal server error' }));
+    }).catch(() => next({ code: 400, msg: 'page not found' }));// validation error
 };
