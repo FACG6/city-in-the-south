a59f734d41ab125f6f7a0791b83d98fc5adb1006
AymanAlQoqa <aalqouqa@gmail.com>
2019-06-12 13:23:15 +0300

    include the patchHiredMember and export it for external usage relates #57

--- server/database/queries/applications/index.js
+++ server/database/queries/applications/index.js
@@ -2,10 +2,12 @@ const addHiredMember = require('./addHiredMember');
 const getOfferApplications = require('./getOfferApplications');
 const getMyApplication = require('./getMyApplication');
 const getMyApplications = require('./getMyApplications');
+const patchHiredMember = require('./patchHiredMember');
 
 module.exports = {
   addHiredMember,
   getOfferApplications,
   getMyApplication,
   getMyApplications,
+  patchHiredMember,
 };
