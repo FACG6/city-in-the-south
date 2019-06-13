a3b8ff2c8c60b17c168f588426a8e309ca85a115
Fatma Siam <fatma.o.siam@gmail.com>
2019-06-13 12:29:27 +0300

    add filter controller with validation Relates: #40

--- server/controllers/filter/patchFilter.js
+++ server/controllers/filter/patchFilter.js
@@ -1,3 +1,22 @@
+
+const yup = require('yup');
+const { patchFilter } = require('../../database/queries/filter/patchFilter');
+
+const filterSchema = yup.object().shape({
+  skills: yup.array()
+    .of(yup.object().shape({ id: yup.number(), name: yup.string() })).required(),
+  offerType: yup.array()
+    .of(yup.object().shape({ id: yup.number(), name: yup.string() })).required(),
+  memberId: yup.number().required(),
+});
 module.exports = (req, res, next) => {
-  res.send('update filter route');
+  const { member_id: memberId } = req.params;
+  const { skills, offer_type: offerType } = req.body;
+  filterSchema.validate({ skills, offerType, memberId })
+    .then(() => {
+      patchFilter(skills, offerType, memberId)
+        .then(result => res.send({ error: null, data: result.rows }))
+        .catch(err => next(err));
+    })
+    .catch(() => next({ code: 400, msg: 'Enter valid data' }));
 };
