ede23ceea3d09f42004ca0976bd63136a41fe531
Fatma Siam <fatma.o.siam@gmail.com>
2019-06-13 13:22:30 +0300

    remove the avatar defult value Relates #35

--- server/database/queries/members/addMember.js
+++ server/database/queries/members/addMember.js
@@ -3,8 +3,8 @@ const dbconnection = require('../../config/db_connection');
 const addMmeber = (memberInfo) => {
   const queryValues = Object.values(memberInfo);
   const sql = {
-    text: 'INSERT INTO member(username,email, pass, avatar) VALUES ( $1, $2, $3, $4 ) RETURNING *',
-    values: [...queryValues, 'https://png.pngtree.com/svg/20161027/631929649c.svg'],
+    text: 'INSERT INTO member(username,email, pass) VALUES ( $1, $2, $3 ) RETURNING *',
+    values: [...queryValues],
   };
   return dbconnection.query(sql);
 };
