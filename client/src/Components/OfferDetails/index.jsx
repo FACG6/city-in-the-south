dd142604dc0596b152a24f26374c59df140b49a2
Angham116 <angham.1161995@hotmail.com>
2019-06-16 10:33:58 +0300

    style: delete commented code

--- client/src/Components/OfferDetails/index.jsx
+++ client/src/Components/OfferDetails/index.jsx
@@ -58,23 +58,6 @@ export default class OfferDetails extends Component {
       .catch(err => console.log('err', err));
   }
 
-  // handleStatusColor = status => {
-  //   switch (status) {
-  //     case 'completed':
-  //     case 'accepted':
-  //       return '#0A8F07';
-  //     case 'pending':
-  //       return '#F77D0E';
-  //     case 'finished':
-  //     case 'refused':
-  //       return 'red';
-  //     case 'inactive':
-  //       return '#1BA7E2';
-  //     default:
-  //       return null;
-  //   }
-  // };
-
   handleEndContract = () => {
     // handle end contract login here....
   };
