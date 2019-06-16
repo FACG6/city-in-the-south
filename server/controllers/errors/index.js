ab42289b45611a89114ffdc424f07b69c000e08c
Ahmed Shatat <a.shatat@hotmail.com>
2019-06-16 11:36:25 +0300

    change error code to type number

--- server/controllers/errors/index.js
+++ server/controllers/errors/index.js
@@ -5,18 +5,18 @@ exports.notFound = (req, res) => {
 exports.serverError = (err, req, res, next) => {
   switch (err.code) {
     case 400:
-      res.status(400).send({ error: { code: '400', msg: err }, data: null });
+      res.status(400).send({ error: { code: 400, msg: err }, data: null });
       break;
     case 401:
-      res.status(401).send({ error: { code: '401', msg: 'authentication error' }, data: null });
+      res.status(401).send({ error: { code: 401, msg: 'authentication error' }, data: null });
 
       break;
     case 403:
-      res.status(403).send({ error: { code: '403', msg: 'forbidden' }, data: null });
+      res.status(403).send({ error: { code: 403, msg: 'forbidden' }, data: null });
 
       break;
     default:
-      res.status(500).send({ error: { code: '500', msg: 'Internal Server Error' }, data: null });
+      res.status(500).send({ error: { code: 500, msg: 'Internal Server Error' }, data: null });
       break;
   }
 };
