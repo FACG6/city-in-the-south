4843d30c9f5c813b77ef5262b59c078e354c69c3
Ahmed <ahmedalmi95@gmail.com>
2019-06-16 15:08:30 +0300

    modified query

--- server/database/queries/members/addMember.js
+++ server/database/queries/members/addMember.js
@@ -1,10 +1,9 @@
 const dbconnection = require('../../config/db_connection');
 
-const addMmeber = (memberInfo) => {
-  const queryValues = Object.values(memberInfo);
+const addMmeber = ({ username, pass, email }) => {
   const sql = {
-    text: 'INSERT INTO member(username,pass, email) VALUES ( $1, $2, $3 ) RETURNING *',
-    values: [...queryValues],
+    text: 'INSERT INTO member(username, pass, email) VALUES ( $1, $2, $3 ) RETURNING *',
+    values: [username, pass, email],
   };
   return dbconnection.query(sql);
 };
