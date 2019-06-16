095303ea742d2a51aeb6a12a28b6ae636761acb9
AymanAlQoqa <aalqouqa@gmail.com>
2019-06-16 14:45:25 +0300

    bug: exchange the email and pass places relates #123

--- server/database/queries/members/addMember.js
+++ server/database/queries/members/addMember.js
@@ -3,7 +3,7 @@ const dbconnection = require('../../config/db_connection');
 const addMmeber = (memberInfo) => {
   const queryValues = Object.values(memberInfo);
   const sql = {
-    text: 'INSERT INTO member(username,email, pass) VALUES ( $1, $2, $3 ) RETURNING *',
+    text: 'INSERT INTO member(username,pass, email) VALUES ( $1, $2, $3 ) RETURNING *',
     values: [...queryValues],
   };
   return dbconnection.query(sql);
