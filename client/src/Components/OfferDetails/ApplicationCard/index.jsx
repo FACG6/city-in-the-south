9301580ccd6340f6016b573a87b1764e034ad3c8
Angham116 <angham.1161995@hotmail.com>
2019-06-16 14:44:19 +0300

    feat: validation schema for apply new application
    #22

--- client/src/Components/OfferDetails/ApplicationCard/index.jsx
+++ client/src/Components/OfferDetails/ApplicationCard/index.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import { Image, Card, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+
+import statusColor from '../../Helper/helper';
 
 import './style.css';
 
@@ -13,8 +16,9 @@ class ApplicationCard extends Component {
   }
 
   handleProfile = () => {
-    // handle profile button
-    // const { member_id: applicantMemberId } = this.props.application;
+    // eslint-disable-next-line react/prop-types
+    const { application: username, history } = this.props;
+    history.push(`/app/profile/${username}`);
   };
 
   handleHireMe = () => {
@@ -24,57 +28,63 @@ class ApplicationCard extends Component {
 
   render() {
     const { application } = this.state;
-    const { defaultAvatar } = this.props;
-    // console.log(application);
+    const { defaultAvatar, viewProfile } = this.props;
     return (
-      <div className="application-card__container">
-        <Card style={{ borderColor: '#eaeaea' }}>
-          <Card.Header>
-            <Card.Title>
-              <Image
-                src={application.avatar ? application.avatar : defaultAvatar}
-                className="application-card__avatar"
-              />
-              <span style={{ paddingLeft: '10px' }}>
-                {application.full_name}
-              </span>
-            </Card.Title>
-          </Card.Header>
-          <Card.Body>
-            <Card.Text style={{ color: 'rgba(51, 51, 51, 0.8)' }}>
-              {application.proposal}
-            </Card.Text>
-            <div className="application-card__button__container">
-              <Button
-                className="application-card__button"
-                onClick={this.handleProfile}
-              >
-                {/* {viewMyProfile ? 'View My Profile' : 'View Profile'} */}
-              </Button>
-              {/* {!status ? (
-                <Button
-                  className="application-card__button"
-                  onClick={this.handleHireMe}
-                >
-                  Hire Me
-                </Button>
-              ) : (
-                <Card.Text style={{ color: statusColor, marginBottom: '5px' }}>
-                  {status.replace(/_/g, ' ').toUpperCase()}
-                </Card.Text>
-              )} */}
-            </div>
-          </Card.Body>
-        </Card>
-      </div>
+      <>
+        <div className="application-card__container">
+          <Card style={{ borderColor: '#eaeaea' }}>
+            <Card.Header>
+              <Card.Title>
+                <Image
+                  src={application.avatar ? application.avatar : defaultAvatar}
+                  className="application-card__avatar"
+                />
+                <span style={{ paddingLeft: '10px' }}>
+                  {application.full_name}
+                </span>
+              </Card.Title>
+            </Card.Header>
+            <Card.Body>
+              <Card.Text style={{ color: 'rgba(51, 51, 51, 0.8)' }}>
+                {application.proposal}
+              </Card.Text>
+              <div className="application-card__button__container">
+                {viewProfile && (
+                  <Button
+                    className="application-card__button"
+                    onClick={this.handleProfile}
+                  >
+                    {' '}
+                    View Profile
+                  </Button>
+                )}
+                {!application.status ? (
+                  <Button
+                    className="application-card__button"
+                    onClick={this.handleHireMe}
+                  >
+                    Hire Me
+                  </Button>
+                ) : (
+                  <Card.Text
+                    className={`status__${statusColor(application.status)}`}
+                  >
+                    {application.status.replace(/_/g, ' ').toUpperCase()}
+                  </Card.Text>
+                )}
+              </div>
+            </Card.Body>
+          </Card>
+        </div>
+      </>
     );
   }
 }
 
-export default ApplicationCard;
+export default withRouter(ApplicationCard);
 
 ApplicationCard.defaultProps = {
-  viewMyProfile: false,
+  viewProfile: false,
 };
 
 ApplicationCard.propTypes = {
@@ -85,5 +95,6 @@ ApplicationCard.propTypes = {
     proposal: PropTypes.string.isRequired,
     // status: PropTypes.string.isRequired,
   }).isRequired,
-  viewMyProfile: PropTypes.bool,
+  viewProfile: PropTypes.bool,
+  defaultAvatar: PropTypes.string.isRequired,
 };
