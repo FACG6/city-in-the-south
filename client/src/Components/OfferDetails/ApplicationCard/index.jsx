961fa2da7cc8300a82ad9a30736300a93702e6b1
Angham116 <angham.1161995@hotmail.com>
2019-06-17 11:57:34 +0300

    refactor: refactor front-end codes on application Card
    add some conditions for define buttons(Accept, refuse or hire me)

--- client/src/Components/OfferDetails/ApplicationCard/index.jsx
+++ client/src/Components/OfferDetails/ApplicationCard/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import { Image, Card, Button } from 'react-bootstrap';
+import { Image, Card, Button, Alert } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
 import statusColor from '../../Helper/helper';
@@ -8,9 +8,22 @@ import statusColor from '../../Helper/helper';
 import './style.css';
 
 class ApplicationCard extends Component {
-  state = { application: '' };
+  state = {
+    application: '',
+    userInfo: '',
+    showWrongAlert: false,
+  };
 
   componentDidMount() {
+    // from localStorage
+    const userInfo = {
+      id: 1,
+      fullName: 'Ayman AlQoqa',
+      username: 'Ayman321396',
+      avatar:
+        'https://m.media-amazon.com/images/M/MV5BMTcxOTk4NzkwOV5BMl5BanBnXkFtZTcwMDE3MTUzNA@@._V1_.jpg',
+    };
+    this.setState({ userInfo });
     const { application } = this.props;
     this.setState({ application });
   }
@@ -23,14 +36,60 @@ class ApplicationCard extends Component {
 
   handleHireMe = () => {
     // handle hire me button
-    // const { member_id: applicantMemberId } = this.props.application;
+    const { application, match, history } = this.props;
+    const { member_id } = application;
+    const {
+      // eslint-disable-next-line react/prop-types
+      params: { offerId },
+    } = match;
+    // fetch('/hired-member', )
+    fetch('/api/v1/hired-member', {
+      method: 'POST',
+      credentials: 'same-origin',
+      body: JSON.stringify({
+        member_id,
+        offer_id: offerId,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => res.json())
+      .then(res => {
+        if(res.data){
+          const { offer_id } = res.data[0];
+          history.push(`/app/my-offers`);
+        }
+      })
+      .catch(() =>
+        this.setState(
+          {
+            showWrongAlert: true,
+          },
+          () =>
+            setTimeout(() => {
+              this.setState({ showWrongAlert: false });
+            }, 5000)
+        )
+      );
+  };
+
+  handleAccept = () => {
+    // send req to update application status(applicaton: accepted, offer:completed)
+    // /api/v1/hired-member/2
+    // fetch()
+  };
+
+  handleRefuse = () => {
+    // send req to update application status(application: refused)
   };
 
   render() {
-    const { application } = this.state;
-    const { defaultAvatar, viewProfile } = this.props;
+    const { application, userInfo, showWrongAlert } = this.state;
+    const { defaultAvatar, viewProfile, hireMe } = this.props;
     return (
       <>
+        {showWrongAlert && <Alert> Somthing went error! Try agailn </Alert>}
         <div className="application-card__container">
           <Card style={{ borderColor: '#eaeaea' }}>
             <Card.Header>
@@ -59,19 +118,41 @@ class ApplicationCard extends Component {
                   </Button>
                 )}
                 {!application.status ? (
-                  <Button
-                    className="application-card__button"
-                    onClick={this.handleHireMe}
-                  >
-                    Hire Me
-                  </Button>
+                  <>
+                    {hireMe && (
+                      <Button
+                        className="application-card__button"
+                        onClick={this.handleHireMe}
+                      >
+                        Hire Me
+                      </Button>
+                    )}
+                  </>
                 ) : (
                   <Card.Text
                     className={`status__${statusColor(application.status)}`}
                   >
-                    {application.status.replace(/_/g, ' ').toUpperCase()}
+                    {application.status}
                   </Card.Text>
                 )}
+                {userInfo.id === application.id &&
+                  application.status &&
+                  application.status === 'pending' && (
+                    <>
+                      <Button
+                        className="application-card__button"
+                        onClick={this.handleAccept}
+                      >
+                        Accept
+                      </Button>
+                      <Button
+                        className="application-card__button"
+                        onClick={this.handleRefuse}
+                      >
+                        Refuse
+                      </Button>
+                    </>
+                  )}
               </div>
             </Card.Body>
           </Card>
@@ -85,6 +166,7 @@ export default withRouter(ApplicationCard);
 
 ApplicationCard.defaultProps = {
   viewProfile: false,
+  hireMe: false,
 };
 
 ApplicationCard.propTypes = {
@@ -95,6 +177,8 @@ ApplicationCard.propTypes = {
     proposal: PropTypes.string.isRequired,
     // status: PropTypes.string.isRequired,
   }).isRequired,
+  applicantMemberId: PropTypes.number.isRequired,
   viewProfile: PropTypes.bool,
+  hireMe: PropTypes.bool,
   defaultAvatar: PropTypes.string.isRequired,
 };
