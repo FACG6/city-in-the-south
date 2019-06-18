0567c14dd03415d0aec716a00e6b094b5f2631b5
Fatma Siam <fatma.o.siam@gmail.com>
2019-06-18 13:41:54 +0300

    make patch full name controller

--- server/controllers/member/patchFullName.js
+++ server/controllers/member/patchFullName.js
@@ -1,3 +1,23 @@
+const yup = require('yup');
+
+const { patcFullName } = require('../../database/queries/members/patchFullName');
+
+const patchFullNameSchema = yup.object().shape({
+  fullname: yup.string().required(),
+  memberId: yup.number().required(),
+});
+
 module.exports = (req, res, next) => {
-  res.send('ho from patch full name');
+  const { memberId } = req.params;
+  const { fullname } = req.body;
+  patchFullNameSchema.validate({ fullname, memberId })
+    .then(() => {
+      patcFullName(fullname, memberId)
+        .then(({ rows }) => {
+          if (!rows[0]) return next({ code: 400, msg: 'Bad Request ..' });
+          return res.status(200).send({ error: null, data: rows[0].full_name });
+        })
+        .catch(err => next(err));
+    })
+    .catch(() => next({ code: 400, msg: 'Ensure you enter validly data' }));
 };
