9d7958cf14782ce9e069c3fec48704fcde6f9913
Fatma Siam <fatma.o.siam@gmail.com>
2019-06-18 14:21:42 +0300

    make update education controller

--- server/controllers/member/updateEducation.js
+++ server/controllers/member/updateEducation.js
@@ -1,3 +1,20 @@
+const { updateEduction } = require('../../database/queries/members/updateEducation');
+const { educationSchema } = require('../../helpers/validation-schema');
+
+
 module.exports = (req, res, next) => {
-  res.send('ho from updateEducarion');
+  const { memberId } = req.params;
+  const {
+    id, title, date, university, description,
+  } = req.body;
+  const educationInfo = {
+    id, title, date, university, description, memberId,
+  };
+  educationSchema.validate(educationInfo)
+    .then(() => {
+      updateEduction(educationInfo, memberId)
+        .then(({ rows }) => res.status(200).send({ error: null, data: rows }))
+        .catch(err => next(err));
+    })
+    .catch(() => next({ code: 400, msg: 'Ensure you enter validly data' }));
 };
