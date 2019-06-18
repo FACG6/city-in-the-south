f3d9d4286d8244f58d03b6122ac68a5f9b6807cc
Fatma Siam <fatma.o.siam@gmail.com>
2019-06-18 14:37:57 +0300

    edit the update experience query

--- server/database/queries/members/updateExperience.js
+++ server/database/queries/members/updateExperience.js
@@ -1,12 +1,12 @@
 const dbconnection = require('../../config/db_connection');
 
-exports.updateExp = (educationInfo, memberId) => {
+exports.updateExperience = (expInfo, memberId) => {
   const {
-    title, date, university, description, id,
-  } = educationInfo;
+    title, endDate, startDate, location, description, id,
+  } = expInfo;
   const sql = {
-    text: 'PDATE experience SET title = $1, end_date = $2, start_date= $3, location = $4, description = $5 WHERE member_id = $6 AND id = $7 RETURNING *',
-    values: [title, date, university, description, memberId, id],
+    text: 'UPDATE experience SET title = $1, end_date = $2, start_date= $3, location = $4, description = $5 WHERE member_id = $6 AND id = $7 RETURNING *',
+    values: [title, endDate, startDate, location, description, memberId, id],
   };
   return dbconnection.query(sql);
 };
