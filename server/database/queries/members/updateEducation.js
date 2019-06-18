963dd31fece05366807609b1909f23ef56b2c964
Fatma Siam <fatma.o.siam@gmail.com>
2019-06-18 12:15:53 +0300

    edit queries

--- server/database/queries/members/updateEducation.js
+++ server/database/queries/members/updateEducation.js
@@ -5,7 +5,7 @@ exports.updateEduction = (educationInfo, memberId) => {
     title, date, university, description, id,
   } = educationInfo;
   const sql = {
-    text: 'UPDATE education SET title = 1$ , date = $2 ,university = $3,  description= $4 WHERE member_id = $5 AND id = $6 RETURNING *',
+    text: 'UPDATE education SET title = 1$ , date = $2 ,university = $3, description= $4 WHERE member_id = $5 AND id = $6 RETURNING *',
     values: [title, date, university, description, memberId, id],
   };
   return dbconnection.query(sql);
--- server/database/queries/members/updateExperience.js
+++ server/database/queries/members/updateExperience.js
@@ -5,7 +5,7 @@ exports.updateExp = (educationInfo, memberId) => {
     title, date, university, description, id,
   } = educationInfo;
   const sql = {
-    text: 'PDATE experience SET title = $1, end_date = $2, start_date= $3, location = $4,  description = $5 WHERE member_id = $6 AND id = $7 RETURNING *',
+    text: 'PDATE experience SET title = $1, end_date = $2, start_date= $3, location = $4, description = $5 WHERE member_id = $6 AND id = $7 RETURNING *',
     values: [title, date, university, description, memberId, id],
   };
   return dbconnection.query(sql);
