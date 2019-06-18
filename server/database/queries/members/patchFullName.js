5a28988fca7e6ae5681aca515308f040a393cb20
Fatma Siam <fatma.o.siam@gmail.com>
2019-06-18 13:58:36 +0300

    edit the typo

--- server/database/queries/members/patchFullName.js
+++ server/database/queries/members/patchFullName.js
@@ -1,6 +1,6 @@
 const dbconnection = require('../../config/db_connection');
 
-exports.patcFullName = (fullName, memberId) => {
+exports.patchFullName = (fullName, memberId) => {
   const sql = {
     text: 'UPDATE member SET full_name = $1 WHERE id= $2 RETURNING *',
     values: [fullName, memberId],
