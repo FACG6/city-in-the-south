aac970884a6e2b78a58f61ff9ec56621600a0614
Fatma Siam <fatma.o.siam@gmail.com>
2019-06-19 13:35:16 +0300

    edit getMember query to get member by username

--- server/database/queries/members/getMember.js
+++ server/database/queries/members/getMember.js
@@ -1,7 +1,7 @@
 
 const dbconnection = require('../../config/db_connection');
 
-exports.getMember = memberId => dbconnection.query(`SELECT member.* , COALESCE(json_agg(skill) FILTER (WHERE skill.id IS NOT NULL),'[]') AS skills
+exports.getMember = username => dbconnection.query(`SELECT member.* , COALESCE(json_agg(skill) FILTER (WHERE skill.id IS NOT NULL),'[]') AS skills
 FROM member LEFT OUTER JOIN member_skill ON member_skill.member_id = member.id 
 LEFT OUTER JOIN skill ON skill.id = member_skill.skill_id 
-GROUP BY member.id HAVING  member.id = $1 `, [memberId]);
+GROUP BY member.id HAVING  member.username = $1 `, [username]);
