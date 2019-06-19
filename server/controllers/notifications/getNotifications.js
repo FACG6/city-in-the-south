61a7cb2a9cb8d8ed17c99b4e0fe61d74c0f39299
AymanAlQoqa <aalqouqa@gmail.com>
2019-06-19 08:13:00 +0300

    feat: add getNotifications controller file to handle /notifications/member_id route relates #137

---
+++ server/controllers/notifications/getNotifications.js
@@ -0,0 +1,19 @@
+const { getNotifications } = require('../../database/queries/notifications');
+
+module.exports = (req, res, next) => {
+  const { member_id: memberId } = req.params;
+  getNotifications(memberId)
+    .then((result) => {
+      if (result.rowCount) {
+        res.send({
+          data: result.rows,
+          error: null,
+        });
+        return;
+      }
+      next({ code: 500, msg: 'Internal Server Error' });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
