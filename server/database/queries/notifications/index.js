a3d5a101a93d571ae324b6f4fc3d548e934d6639
AymanAlQoqa <aalqouqa@gmail.com>
2019-06-19 07:09:51 +0300

    feat: collect all notification queries in single index file relates #137

---
+++ server/database/queries/notifications/index.js
@@ -0,0 +1,9 @@
+const getOfferTitleAndMemberUsername = require('./getOfferTitleAndMemberUsername');
+const addNewNotification = require('./addNewNotification');
+const patchNotification = require('./patchNotification');
+
+module.exports = {
+  getOfferTitleAndMemberUsername,
+  addNewNotification,
+  patchNotification,
+};
