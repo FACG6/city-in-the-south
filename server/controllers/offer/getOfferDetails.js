547dfbfbb883c03c4a7dbd7ac3b2d03e321188a4
AymanAlQoqa <aalqouqa@gmail.com>
2019-06-20 12:07:48 +0300

    mod: modify getOfferDetails to return offer detiles including skills and offer_types relates #164

--- server/controllers/offer/getOfferDetails.js
+++ server/controllers/offer/getOfferDetails.js
@@ -1,8 +1,19 @@
-const { getOffer } = require('./../../database/queries/offers/getOfferId');
+const getOffer = require('./../../database/queries/offers/getOfferId');
+const { getOfferOfferType } = require('../../database/queries/offer-type/getOfferOfferType');
+const { getOfferSkills } = require('../../database/queries/skills');
 
 module.exports = (req, res, next) => {
   const { offerId } = req.params;
-  getOffer(offerId)
-    .then(({ rows }) => res.status(200).send({ error: null, data: rows[0] }))
+  Promise.all([getOffer(offerId), getOfferSkills(offerId), getOfferOfferType(offerId)])
+    .then((values) => {
+      const offer = values[0].rows[0];
+      const skills = values[1].rows;
+      const offerType = values[2].rows;
+      offer.skills = skills;
+      offer.offer_types = offerType;
+      res.status(200).send({ error: null, data: offer });
+    })
     .catch(err => next(err));
 };
+
+// res.status(200).send({ error: null, data: rows[0] });
