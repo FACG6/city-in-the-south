ccb0da494a486c6f1fe054b497924e04a32fd2d0
Fatma Siam <fatma.o.siam@gmail.com>
2019-06-20 12:54:44 +0300

    refactor the saved offer controller

--- server/controllers/offer/addSavedOffer.js
+++ server/controllers/offer/addSavedOffer.js
@@ -1,14 +1,23 @@
 const { SavedOfferSchema } = require('../../helpers/validation-schema');
 
 const { addSavedOffers } = require('../../database/queries/offers/saved-offers');
+const { checkOffers } = require('../../database/queries/offers/getSavedOffer');
 
 module.exports = (req, res, next) => {
   const savedOfferInfo = req.body;
+  const { memberId, offerId } = req.body;
   SavedOfferSchema
     .validate(savedOfferInfo)
     .then(() => {
-      addSavedOffers(savedOfferInfo)
-        .then(({ rows: savedOffers }) => res.status(200).send({ error: null, data: savedOffers }))
+      checkOffers(memberId, offerId)
+        .then(({ rows }) => {
+          if (!rows[0]) {
+            addSavedOffers(savedOfferInfo)
+              .then(({ rows: savedOffers }) => res.status(200)
+                .send({ error: null, data: savedOffers }))
+              .catch(err => next(err));
+          } else next({ code: 400, msg: 'this offer already saved' });
+        })
         .catch(err => next(err));
     })
     .catch(() => next({ code: 400, msg: 'Ensure to enter validly data' }));
