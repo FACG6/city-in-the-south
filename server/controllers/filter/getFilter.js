b83d5d6025c13fd9ca3520f12e190f7b11b5776f
Angham116 <angham.1161995@hotmail.com>
2019-06-20 16:30:27 +0300

    fix: add condition for empty data

--- server/controllers/filter/getFilter.js
+++ server/controllers/filter/getFilter.js
@@ -5,11 +5,14 @@ const parseData = arr => arr.map(item => JSON.parse(item));
 module.exports = (req, res, next) => {
   getFilterData(req.params.member_id)
     .then((response) => {
-      const data = response.rows[0];
-      if (data.skills) {
+      let data = response.rows[0];
+      if (!data) {
+        data = {};
+      }
+      if (data && data.skills) {
         data.skills = parseData(data.skills);
       }
-      if (data.offer_type) {
+      if (data && data.offer_type) {
         data.offer_type = parseData(data.offer_type);
       }
       res.send({
