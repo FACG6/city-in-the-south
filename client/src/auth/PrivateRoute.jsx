473a1577b38ed4cf7339a05c3226ba47c0d2bf87
Angham116 <angham.1161995@hotmail.com>
2019-06-21 11:32:20 +0300

    refactor: remove unsed Component from PrivateRoute

--- client/src/auth/PrivateRoute.jsx
+++ client/src/auth/PrivateRoute.jsx
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import auth from './auth';
 
+// eslint-disable-next-line react/prop-types
 const PrivateRoute = ({ component: Component, ...rest }) => {
   return (
     <Route
