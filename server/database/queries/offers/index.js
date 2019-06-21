6726cfc3a3accb8fb42cd261c38d602519f89f02
Angham116 <angham.1161995@hotmail.com>
2019-06-21 20:14:15 +0300

    refactor: split deleteOffer and getOffers queries to external files

--- server/database/queries/offers/index.js
+++ server/database/queries/offers/index.js
@@ -4,17 +4,9 @@ const {
   addOfferTypes,
 } = require('./addNewOffer');
 
-const connection = require('../../config/db_connection');
+const { getOffers } = require('./getOffers');
 
-const getOffers = offset => connection.query(`SELECT offer.* , COALESCE(json_agg(skill) FILTER (WHERE skill.id IS NOT NULL),'[]') AS skills,
-COALESCE(json_agg(offer_type) FILTER (WHERE skill.id IS NOT NULL),'[]') AS offer_types FROM offer
-LEFT OUTER JOIN offer_skill ON offer_skill.offer_id = offer.id
-LEFT OUTER JOIN skill ON skill.id = offer_skill.skill_id
-LEFT OUTER JOIN offer_offer_type ON offer_offer_type.offer_id = offer.id
-LEFT OUTER JOIN offer_type ON offer_type.id = offer_offer_type.offer_type_id
-WHERE offer.status ='active' GROUP BY offer.id LIMIT 100 OFFSET $1`, [offset]);
-
-const deleteOffer = id => connection.query('DELETE FROM offer WHERE offer.id = $1 ', [id]);
+const { deleteOffer } = require('./deleteOffer');
 
 module.exports = {
   addOfferDetails,
