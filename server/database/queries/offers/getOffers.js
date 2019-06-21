c26c181cfa55f83cc90e644783242ef90f8a6f62
Angham116 <angham.1161995@hotmail.com>
2019-06-21 20:11:50 +0300

    refactor: add getOffer query to split file

---
+++ server/database/queries/offers/getOffers.js
@@ -0,0 +1,11 @@
+const connection = require('../../config/db_connection');
+
+const getOffers = offset => connection.query(`SELECT offer.* , COALESCE(json_agg(skill) FILTER (WHERE skill.id IS NOT NULL),'[]') AS skills,
+COALESCE(json_agg(offer_type) FILTER (WHERE skill.id IS NOT NULL),'[]') AS offer_types FROM offer
+LEFT OUTER JOIN offer_skill ON offer_skill.offer_id = offer.id
+LEFT OUTER JOIN skill ON skill.id = offer_skill.skill_id
+LEFT OUTER JOIN offer_offer_type ON offer_offer_type.offer_id = offer.id
+LEFT OUTER JOIN offer_type ON offer_type.id = offer_offer_type.offer_type_id
+WHERE offer.status ='active' GROUP BY offer.id LIMIT 100 OFFSET $1`, [offset]);
+
+module.exports = { getOffers };
