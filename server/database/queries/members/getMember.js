cfe595c873f7280d30952b366bd0d4478375bac9
Angham116 <angham.1161995@hotmail.com>
2019-06-21 22:19:46 +0300

    refactor: remove member password from returning data

--- server/database/queries/members/getMember.js
+++ server/database/queries/members/getMember.js
@@ -1,7 +1,7 @@
 
 const dbconnection = require('../../config/db_connection');
 
-exports.getMember = username => dbconnection.query(`SELECT member.* , COALESCE(json_agg(skill) FILTER (WHERE skill.id IS NOT NULL),'[]') AS skills
+exports.getMember = username => dbconnection.query(`SELECT member.id, member.address, member.avatar, member.bio, member.email, member.full_name, member.phone, member.profession_id, member.username ,COALESCE(json_agg(skill) FILTER (WHERE skill.id IS NOT NULL),'[]') AS skills
 FROM member LEFT OUTER JOIN member_skill ON member_skill.member_id = member.id 
 LEFT OUTER JOIN skill ON skill.id = member_skill.skill_id 
 GROUP BY member.id HAVING  member.username = $1 `, [username]);
