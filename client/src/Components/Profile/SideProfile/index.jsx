fa81e4a647c6175ce6a228b34a21cdf0fa908d08
Angham116 <angham.1161995@hotmail.com>
2019-06-22 18:29:06 +0300

    refactor: component for basic information of member with its style

--- client/src/Components/Profile/SideProfile/index.jsx
+++ client/src/Components/Profile/SideProfile/index.jsx
@@ -1,42 +1,69 @@
-/* eslint-disable react/prop-types */
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Row, Col, Container, Alert } from 'react-bootstrap';
 
 import './style.css';
 
-export default function SideProfile({
-  avatar,
-  username,
-  address,
-  email,
-  phone,
-  skills,
-}) {
-  return (
-    <>
-      <img
-        src={avatar}
-        width="100%"
-        alt={`${username} profile img`}
-        className="profile__img"
-      />
-      <p className="profile__username">{username}</p>
-      <h2 className="profile__title">
-        CONTACT
-        <hr className="profile__title__line" />
-      </h2>
-      <div className="profile__contactInfo">
-        {address && <p>{address}</p>}
-        {email && <p>Email: {email}</p>}
-        {phone && <p>Phone: {phone}</p>}
-      </div>
-      <h2 className="profile__title">
-        SKILLS
-        <hr className="profile__title__line" />
-      </h2>
-      <div className="profile__skills">
-        {skills[0] &&
-          skills.map(skill => <p key={`skill-${skill.id}`}> - {skill.name}</p>)}
-      </div>
-    </>
-  );
+export default class MemberInfo extends Component {
+  state = {
+    userInfo: {},
+    member: '',
+  };
+
+  componentDidMount() {
+    const { member } = this.props;
+    const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+    this.setState({ userInfo, member });
+  }
+
+  render() {
+    const { userInfo, member, showAlert } = this.state;
+    return (
+      <Container>
+        {showAlert && (
+          <Alert variant="danger">Something went error! Try again.</Alert>
+        )}
+        <>
+          <Row>
+            <img
+              src={
+                member.avatar
+                  ? member.avatar
+                  : 'https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1'
+              }
+              width="100%"
+              alt={`${member.username} profile img`}
+              className="profile__img"
+            />
+            <h3 className="profile__username">{member.username}</h3>
+          </Row>
+          <Row>
+            <Col>
+              <h2 className="profile__title">CONTACT</h2>
+              <div className="profile__contactInfo">
+                {member.address && <p>{member.address}</p>}
+                {member.email && <p>Email: {member.email}</p>}
+                {member.phone && <p>Phone: {member.phone}</p>}
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h2 className="profile__title">SKILLS</h2>
+              <div className="profile__skills">
+                {member.skills &&
+                  member.skills.map(skill => (
+                    <h6 key={`skill-${skill.id}`}> - {skill.name}</h6>
+                  ))}
+              </div>
+            </Col>
+          </Row>
+        </>
+      </Container>
+    );
+  }
 }
+
+MemberInfo.propTypes = {
+  // memberName: PropTypes.string.isRequired,
+};
