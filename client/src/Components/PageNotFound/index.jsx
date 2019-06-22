a529b7b2e75dcaee8aaddc8ac0676ff8d5ac7a58
Ahmed <ahmedalmi95@gmail.com>
2019-06-22 04:55:23 +0300

    change the class name

--- client/src/Components/PageNotFound/index.jsx
+++ client/src/Components/PageNotFound/index.jsx
@@ -5,7 +5,7 @@ import './style.css';
 export default function PageNotFound() {
   return (
     <div className="page-not-found">
-      <h1 className="not-found">404</h1>
+      <h1 className="number">404</h1>
       <p className="oops">Oops! Something is wrong.</p>
       <Link to="/" className="go-back">
         Go back in the initial page, is better
