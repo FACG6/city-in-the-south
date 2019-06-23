9eb394cb55d0be92487cb1cf6ad0c6233e908b93
Angham116 <angham.1161995@hotmail.com>
2019-06-23 13:05:23 +0300

    feat: passing full name of member into MemberInfo component

--- client/src/Components/Profile/MemberInfo/index.jsx
+++ client/src/Components/Profile/MemberInfo/index.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
-import { Row } from 'react-bootstrap';
+import { Row, Container } from 'react-bootstrap';
 
 import Education from './Education';
 import Experience from './Experience';
 import Bio from './Bio';
 
 export default function MemberInfo(props) {
-  const { memberName, memberId, bio } = props;
+  const { memberName, memberId, bio, fullName } = props;
   return (
     <>
-      <Row>
-        <Bio memberId={memberId} bio={bio} />
-      </Row>
-      <Row>
-        <Education memberId={memberId} />
-      </Row>
-      <Row>
-        <Experience memberId={memberId} />
-      </Row>
+      <Container className="profile__page">
+        <Row>
+          <h3>{fullName}</h3>
+        </Row>
+        <Row>
+          <Bio memberId={memberId} bio={bio} />
+        </Row>
+        <Row>
+          <Education memberId={memberId} />
+        </Row>
+        <Row>
+          <Experience memberId={memberId} />
+        </Row>
+      </Container>
     </>
   );
 }
