c873ebe071c5e16b04ee37ce37d6b73f0a6a8f77
Angham116 <angham.1161995@hotmail.com>
2019-06-23 16:39:17 +0300

    refactor: refactor delete offer query

--- server/database/queries/offers/deleteOffer.js
+++ server/database/queries/offers/deleteOffer.js
@@ -1,5 +1,5 @@
 const connection = require('../../config/db_connection');
 
-const deleteOffer = id => connection.query('DELETE FROM offer WHERE offer.id = $1 ', [id]);
+const deleteOffer = id => connection.query('DELETE FROM offer WHERE offer.id = $1', [id]);
 
 module.exports = { deleteOffer };
