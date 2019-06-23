6a21cdf2000fd43c33ec9ad7c5cfc8287fcd7a81
Angham116 <angham.1161995@hotmail.com>
2019-06-23 17:15:49 +0300

    refactor: insert member_id to delete offer query

--- server/database/queries/offers/deleteOffer.js
+++ server/database/queries/offers/deleteOffer.js
@@ -1,5 +1,5 @@
 const connection = require('../../config/db_connection');
 
-const deleteOffer = id => connection.query('DELETE FROM offer WHERE offer.id = $1', [id]);
+const deleteOffer = (offerId, memberId) => connection.query('DELETE FROM offer WHERE offer.id = $1 AND offer.member_id = $2', [offerId, memberId]);
 
 module.exports = { deleteOffer };
