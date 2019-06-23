2df1925c6c5da69961f103a51964b6ef979b0fd4
Fatma Siam <fatma.o.siam@gmail.com>
2019-06-23 16:25:14 +0300

    edit page not found style Relates : #193

--- client/src/Components/PageNotFound/index.jsx
+++ client/src/Components/PageNotFound/index.jsx
@@ -5,10 +5,16 @@ import './style.css';
 export default function PageNotFound() {
   return (
     <div className="page-not-found">
+      <br />
+      <br />
+      <br />
+      <br />
+
       <h1 className="number">404</h1>
-      <p className="oops">Oops! Something is wrong.</p>
+      <p className="oops">Oops! Page Not Found ... !</p>
+      <br />
       <Link to="/" className="go-back">
-        Go back in the initial page, is better
+        Go back to Landing page
       </Link>
     </div>
   );
