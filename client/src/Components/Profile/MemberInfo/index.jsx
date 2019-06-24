f94504a297a144c6571b8e00e0560f2e0b0b1b96
Angham116 <angham.1161995@hotmail.com>
2019-06-24 12:32:52 +0300

    refactor: member info style

--- client/src/Components/Profile/MemberInfo/index.jsx
+++ client/src/Components/Profile/MemberInfo/index.jsx
@@ -5,24 +5,18 @@ import Education from './Education';
 import Experience from './Experience';
 import Bio from './Bio';
 
+import './style.css';
+
 export default function MemberInfo(props) {
-  const { memberName, memberId, bio, fullName } = props;
+  const { memberId, bio, fullName } = props;
   return (
-    <>
-      <Container className="profile__page">
-        <Row>
-          <h3>{fullName}</h3>
-        </Row>
-        <Row>
-          <Bio memberId={memberId} bio={bio} />
-        </Row>
-        <Row>
-          <Education memberId={memberId} />
-        </Row>
-        <Row>
-          <Experience memberId={memberId} />
-        </Row>
-      </Container>
-    </>
+    <div className='profile__page'>
+      <h3 className='fullname'>{fullName}</h3>
+      <Bio memberId={memberId} bio={bio} />
+      <hr />
+      <Education memberId={memberId} />
+      <hr />
+      <Experience memberId={memberId} />
+    </div>
   );
 }
