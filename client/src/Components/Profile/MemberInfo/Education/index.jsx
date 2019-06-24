de2b43dd4c7cfce80b893a592df1ceeca3ee30fe
Angham116 <angham.1161995@hotmail.com>
2019-06-24 12:31:32 +0300

    refactor: refactor member education and rfactor its date

--- client/src/Components/Profile/MemberInfo/Education/index.jsx
+++ client/src/Components/Profile/MemberInfo/Education/index.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Alert, Spinner } from 'react-bootstrap';
 
 import './style.css';
 
 // eslint-disable-next-line react/prop-types
 export default class Education extends Component {
   state = {
-    educations: '',
+    educations: [],
     memberId: '',
     showAlert: false,
+    eduError: false,
   };
 
   componentDidMount() {
@@ -17,11 +18,9 @@ export default class Education extends Component {
     fetch(`/api/v1/education/${memberId}`, { method: 'GET' })
       .then(res => res.json())
       .then(res => {
-        if (res.data) {
+        if (res.data.length) {
           this.setState({ educations: res.data });
-        } else {
-          throw new Error();
-        }
+        } else this.setState({ eduError: true });
       })
       .catch(() => {
         this.setState({ showAlert: true }, () =>
@@ -33,25 +32,53 @@ export default class Education extends Component {
   }
 
   render() {
-    const { educations } = this.state;
+    const { educations, showAlert, eduError } = this.state;
     return (
       <Row>
-        <Col lg={4} md={4}>
+        <Col>
+          {showAlert ? <Alert>Somthing went error ! Try again.</Alert> : null}
           <h2 className="profile__title">EDUCATION</h2>
-          <br />
-          <div>
-            <h3 className="profile__education_title">{educations.title}</h3>
-          </div>
-          <p className="profile__education_date">{educations.date}</p>
-        </Col>
-        <Col lg={8} md={8}>
-          <br />
-          <div>
-            <span className="profile__education__un">
-              {educations.university}
-            </span>
-          </div>
-          <p className="profile_education_desc">{educations.description}</p>
+          {!eduError && !educations[0] && (
+            <Spinner animation="grow" variant="info" />
+          )}
+          {eduError && !educations[0] && (
+            <>
+              <Col lg={12}>
+                <h5 className="profile__empty-msg"> There is no educations </h5>
+              </Col>
+            </>
+          )}
+          {!eduError &&
+            educations[0] &&
+            educations.map(education => (
+              <Row>
+                <Col lg={5} md={4}>
+                  <br />
+                  <div>
+                    <h3 className="profile__education_title">
+                      {education.title}
+                    </h3>
+                  </div>
+                  {/* <p className="profile__education_date">
+                    {new Date(education.date).toLocaleDateString()}
+                  </p> */}
+                </Col>
+                <Col lg={7} md={8}>
+                  <br />
+                  <div>
+                    <span className="profile__education__un">
+                      {education.university} 
+                      <span className="profile__education_date">
+                        {new Date(education.date).toLocaleDateString()}
+                      </span>
+                    </span>
+                  </div>
+                  <p className="profile_education_desc">
+                    {education.description}
+                  </p>
+                </Col>
+              </Row>
+            ))}
         </Col>
       </Row>
     );
