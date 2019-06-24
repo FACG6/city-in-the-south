526ff7bbfb2dca90c3276e52545ad65244746308
AymanAlQoqa <aalqouqa@gmail.com>
2019-06-24 12:11:51 +0300

    return the request body back if the member is guest relates #198

--- server/controllers/filter/patchFilter.js
+++ server/controllers/filter/patchFilter.js
@@ -4,16 +4,13 @@ const { patchFilter } = require('../../database/queries/filter/patchFilter');
 const parseData = arr => arr.map(item => JSON.parse(item));
 
 const filterSchema = yup.object().shape({
-  skills: yup
-    .array()
-    .of(yup.object().shape({ id: yup.number(), name: yup.string() })),
-  offerType: yup
-    .array()
-    .of(yup.object().shape({ id: yup.number(), name: yup.string() })),
-  memberId: yup.number().required(),
+  skills: yup.array().of(yup.object().shape({ id: yup.number(), name: yup.string() })),
+  offerType: yup.array().of(yup.object().shape({ id: yup.number(), name: yup.string() })),
+  memberId: yup.string().required(),
 });
 module.exports = (req, res, next) => {
   const { member_id: memberId } = req.params;
+  if (memberId === 'guest') return res.send({ error: null, data: req.body });
   const { skills, offer_type: offerType } = req.body;
   filterSchema
     .validate({ skills, offerType, memberId })
